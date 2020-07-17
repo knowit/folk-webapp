b1494c6730798c541894d96c6420177a8a8754dd
Jens Kristoffer Retian Markussen <jens.markussen@knowit.no>
2020-07-17 11:16:16 +0200

    dropdown styling

--- src/components/DropdownPicker.tsx
+++ src/components/DropdownPicker.tsx
@@ -1,60 +1,126 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { 
-    NativeSelect,
-    InputBase
+    Select,
+    InputBase,
+    MenuItem,
+    Grow
 } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles'
 
-type ValueType = {displayValue: any, value: any} | any
+type ValueType = {displayValue: string, value: any} | string
 
 interface DropdownPickerProps {
     values: ValueType[],
-    onChange?: (newValue: ValueType) => void 
+    onChange?: (newValue: ValueType | undefined) => void 
 }
 
 const useStyles = makeStyles({
-    root: {
+    root: ({ width } : { width: number }) => ({
         backgroundColor: '#F1F0ED',
         fontSize: 16,
-        height: 43,
         border: '1px solid white',
-        '& > svg': {
-            right: 10
-        },
-        '& $select': {
-            backgroundColor: '#F1F0ED !important',
-        }
-    },
+        height: 43,
+        width: width,
+    }),
     input: {
+        paddingLeft: 12,
         paddingTop: 0,
         paddingBottom: 0,
+        lineHeight: '43px',
+        height: 'inherit',
+    },
+    menu: {},
+    menuPaper: ({ width } : { width: number }) => ({
+        borderRadius: 0,
+        width: width,
+        backgroundColor: '#F1F0ED',
+    }),
+    menuList: {
+        borderLeft: '1px solid white',
+        borderRight: '1px solid white',
+    },
+    menuItem: {
+        fontSize: 14,
+        borderBottom: '1px solid white',
         paddingLeft: 12,
-        paddingRight: '34px !important',
-        height: 'inherit'
     }
 })
 
+const useMuiOverrideStyles = makeStyles({
+    icon: {
+        color: '#333333',
+        right: 10
+    },
+    select: {
+        backgroundColor: '#F1F0ED !important',
+    }
+})
+
+
+const getDisplayValue = (value: ValueType) : string => typeof(value) === 'string' ? value : value.displayValue
+const getValue = (value: ValueType) : any => typeof(value) === 'string' ? value : value.value
+
+
+function measureTextWidth(text: string, font: string ) : number{
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+    if (!context) return 0
+
+    context.font = font;
+    return context.measureText(text).width;
+}
 
 export default function DropdownPicker({
     values,
     onChange = () => null 
 } : DropdownPickerProps) {
-    const classes = useStyles()
+    const width = measureTextWidth(
+        values.map(x => getDisplayValue(x))
+        .reduce((prev, next) => prev.length > next.length ? prev : next), '16pt arial') + 46
+
+    const classes = useStyles({ width })
+    const overrideClasses = useMuiOverrideStyles()
+    const selectRef = useRef<HTMLElement | null>();
 
     return (
-        <NativeSelect
+        <Select
             className={classes.root}
+            classes={overrideClasses}
             variant='standard'
+            inputRef={selectRef}
+            autoWidth={true}
             // eslint-disable-next-line
-            onChange={({ target: { value } }) => onChange( values.find(x => x.value === value || x == value) )}
+            onChange={({ target: { value } }) => onChange( values.find(x => (value as any) == getValue(x)) )}
             input={<InputBase/>}
+            defaultValue={getDisplayValue(values[0])}
             inputProps={{
                 className: classes.input
+            }}
+            MenuProps={{
+                TransitionComponent: Grow,
+                className: classes.menu,
+                MenuListProps: {
+                    disablePadding: true,
+                    className: classes.menuList
+                },
+                PaperProps: {
+                    className: classes.menuPaper
+                },
+                getContentAnchorEl: null,
+                anchorOrigin:{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                },
+                transformOrigin: {
+                    vertical: 'top',
+                    horizontal: 'left',
+                },
+                elevation: 0
             }}>
             
             {values.map((x : ValueType) => (
-                <option value={x.value || x}>{x.displayValue || x}</option>
+                <MenuItem className={classes.menuItem} value={getValue(x)}>{getDisplayValue(x)}</MenuItem>
             ))}
-        </NativeSelect>
+        </Select>
     )
 }
