e9d1733a46c3dcf689c8a322bd6274b0fb2cce81
Alireza Balouch <swape@swape.net>
2020-08-14 12:01:32 +0200

    formatting the backend files

--- backend/edge.js
+++ backend/edge.js
@@ -1,10 +1,9 @@
-
 exports.adaptiveContent = async (event) => {
-    const { request } = event.Records[0].cf
+  const { request } = event.Records[0].cf;
 
-    // if (headers['cloudfront-is-mobile-viewer'] && headers['cloudfront-is-mobile-viewer'][0].value === 'true') {
-    //     request.uri = '/small' + request.uri;
-    // }
+  // if (headers['cloudfront-is-mobile-viewer'] && headers['cloudfront-is-mobile-viewer'][0].value === 'true') {
+  //     request.uri = '/small' + request.uri;
+  // }
 
-    return request
-}
+  return request;
+};
