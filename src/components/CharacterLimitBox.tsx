1ad8792267a16147ed5413460355349aa487ddb1
fridaklockmann <fridaklockmann@gmail.com>
2020-09-24 09:52:08 +0200

    CharacterLim as a class

--- src/components/CharacterLimitBox.tsx
+++ src/components/CharacterLimitBox.tsx
@@ -1,4 +1,4 @@
-import React  from 'react';
+import * as React  from 'react';
 import { withStyles, makeStyles} from '@material-ui/core/styles';
 import Tooltip from '@material-ui/core/Tooltip';
 
@@ -26,7 +26,7 @@ const useBoxStyle = makeStyles({
 
  
 
-export default function CharacterLimitBox({ text }: { text: string;}) {
+ function CharacterLimitBox1({ text }: { text: string;}) {
   const classes = useBoxStyle();
   return (
     <HtmlTooltip title = {text} arrow placement="top">
@@ -36,3 +36,52 @@ export default function CharacterLimitBox({ text }: { text: string;}) {
     </HtmlTooltip>
   );
 }
+
+
+interface Props {
+  text:string;
+  lim?:number;
+}
+
+interface State {
+  overflowActive:boolean;
+}
+const initialState: State = {
+  overflowActive: false
+};
+
+export default class CharacterLimitBox extends React.Component<Props, State> {
+  span: HTMLSpanElement | null | undefined;
+  constructor(props:Props) {
+    super(props);
+    this.state = initialState
+  }
+
+ isEllipsisActive(e:any):boolean {
+  return e.offsetHeight < e.scrollHeight || e.offsetWidth < e.scrollWidth;
+}
+
+componentDidMount():void {
+  this.setState({ overflowActive: this.isEllipsisActive(this.span) });
+}
+  render(){
+    return  <div
+            style={{
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+              overflow: "hidden"
+            }}
+            ref={ref => (this.span = ref)}
+            >
+             <HtmlTooltip title = {this.props.text} arrow placement="top">
+                <span>
+                  {this.props.text}
+                </span>
+              </HtmlTooltip>
+              {console.log(this.state.overflowActive)}
+          </div>
+  }
+}
+
+
+
