822da84d5a443954b6964edd7fec14774eeecbf1
fridaklockmann <fridaklockmann@gmail.com>
2020-10-01 12:08:06 +0200

    added ErrorText component

---
+++ src/components/ErrorText.tsx
@@ -0,0 +1,23 @@
+import { makeStyles } from '@material-ui/core'
+import React from 'react'
+
+const useErrorStyle = makeStyles({
+    root:{
+        color: "#802826",
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        paddingBottom: 25,
+    },
+})
+
+export const ErrorText = () => {
+    const classes = useErrorStyle();
+    return(
+        <div className ={classes.root}>
+            <h2> Oida, en feil har oppstått</h2>
+            <div> Data kan ikke vises for dette valget</div>
+        </div>
+    );
+}
