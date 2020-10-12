b7c4c5546d1859e3a8e55fabea79bfcdc5102440
fridaklockmann <fridaklockmann@gmail.com>
2020-10-12 15:41:12 +0200

    added colors to theme

--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,24 @@ import { BrowserRouter } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
-const theme = createMuiTheme({
+export const theme = createMuiTheme({
   palette: {
+    primary:{
+      main: '#707070',
+    },
     background: {
       default: '#FFFFFF',
+      paper: '#F1F0ED',
     },
     text: {
       primary: '#333333',
     },
+    error:{
+      main: '#802826',
+    },
+    success:{
+      main: '#1e561f',
+    }
   },
   spacing: 22.5,
   typography: {
