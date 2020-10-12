05f164dee54ab4b0fcac8e73224bf59aa50878f5
fridaklockmann <fridaklockmann@gmail.com>
2020-10-12 14:41:16 +0200

    added color from theme

--- src/components/ErrorText.tsx
+++ src/components/ErrorText.tsx
@@ -1,32 +1,37 @@
-import { makeStyles } from '@material-ui/core'
 import React from 'react'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import {theme} from '../../src/index'
 
-const useErrorStyle = makeStyles({
-    root:{
-        color: "#802826",
-        width: '100%',
-        display: 'flex',
-        flexDirection:'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height:280,
-        
-    },
-    titleText:{
-        fontSize: '18px',
-        fontWeight: 'bold',
-        lineHeight: 1.28,
-    },
-    text:{
-        fontSize: '16px',
-        lineHeight: 1.5,
-    }
-})
+const useErrorStyle = makeStyles((theme: Theme) =>
+    createStyles({
+        root:{
+            width: '100%',
+            display: 'flex',
+            flexDirection:'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height:280,
+            color: theme.palette.error.main,
+            
+        },
+        titleText:{
+            fontSize: '18px',
+            fontWeight: 'bold',
+            lineHeight: 1.28,
+        },
+        text:{
+            fontSize: '16px',
+            lineHeight: 1.5,
+        }
+    })
+);
+
 export const NoData = () => (
     <div title = "Data ikke tilgjengelig">-</div>
 )
 export const ErrorText = () => {
-    const classes = useErrorStyle();
+    const classes = useErrorStyle(theme);
+
     return(
         <div className ={classes.root}>
             <div className ={classes.titleText}> Oida, en feil har oppstått</div>
