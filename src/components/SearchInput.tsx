7820a62abda312db1c92649bc897960ac7062391
Alireza Balouch <swape@swape.net>
2020-10-22 13:26:21 +0200

    adding close button to search

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { InputBase, InputAdornment, Theme } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 interface SearchInputProps {
   onChange?: (newValue: string) => void;
 }
 
-const useStyles = makeStyles((theme:Theme)=>({
+const useStyles = makeStyles((theme: Theme) => ({
   root: {
     backgroundColor: 'white',
     height: 43,
@@ -18,23 +20,57 @@ const useStyles = makeStyles((theme:Theme)=>({
   },
   icon: {
     color: theme.palette.primary.main,
-  }
+    borderRadius: 0,
+    transition: 'none',
+    padding: 0,
+    '&:hover': {
+      backgroundColor: 'transparent',
+    },
+  },
 }));
 
 export default function SearchInput({
   onChange = () => null,
 }: SearchInputProps) {
   const classes = useStyles();
+  const [val, setVal] = React.useState('');
+
+  const changeValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    setValueAndChange(event.target.value);
+  };
+
+  const setValueAndChange = (value: string) => {
+    setVal(value);
+    onChange(value);
+  };
 
   return (
-    <InputBase
-      className={classes.root}
-      onChange={({ target: { value } }) => onChange(value)}
-      endAdornment={
-        <InputAdornment position="end">
-          <SearchIcon className={classes.icon}/>
-        </InputAdornment>
-      }
-    />
+    <>
+      <InputBase
+        className={classes.root}
+        onChange={changeValue}
+        value={val}
+        type="text"
+        name="search"
+        placeholder="Søk konsulent, kunde..."
+        endAdornment={
+          <InputAdornment position="end">
+            {val === '' ? (
+              <SearchIcon className={classes.icon} />
+            ) : (
+              <IconButton
+                color="inherit"
+                className={classes.icon}
+                aria-label="Tøm"
+                onClick={() => setValueAndChange('')}
+              >
+                <CloseIcon />
+              </IconButton>
+            )}
+          </InputAdornment>
+        }
+      />
+    </>
   );
 }
