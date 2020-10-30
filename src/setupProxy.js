6fc36f3a8a4f6a326c58485d398152c3eb3504bb
Alireza Balouch <swape@swape.net>
2020-10-30 11:52:58 +0100

    ignore some issues with module export

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/camelcase */
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const config = require('../package.json');
 
+// eslint-disable-next-line func-names
 module.exports = function (app) {
   const proxy_url = process.env.PROXY_URL || config.proxy;
   const proxy = createProxyMiddleware({
