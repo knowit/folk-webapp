c3562102cdb01a86f38a3c32f21d3e45c7f4df69
Alireza Balouch <swape@swape.net>
2020-10-30 11:51:56 +0100

    adding env and rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
   extends: ['airbnb-typescript-prettier'],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    commonjs: true,
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+  },
 };
