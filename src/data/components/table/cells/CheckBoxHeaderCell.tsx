154cb3f16cffd7a8868fa4b6b2f04e1e38ea0935
fridaklockmann <fridaklockmann@gmail.com>
2020-11-06 11:16:25 +0100

    rebased

--- src/data/components/table/cells/CheckBoxHeaderCell.tsx
+++ src/data/components/table/cells/CheckBoxHeaderCell.tsx
@@ -1,58 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { FormControlLabel } from '@material-ui/core';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
-import Checkbox, { CheckboxProps } from '@material-ui/core/Checkbox';
-
-const BlackCheckBox = withStyles({
-  root: {
-    color: '#333333',
-    '&$checked': {
-      color: '#333333',
-    },
-    '&:hover': {
-      backgroundColor: 'transparent',
-    },
-  },
-  checked: {},
-})((props: CheckboxProps) => (
-  <Checkbox color="default" disableRipple {...props} />
-));
-
-const useCheckBoxStyles = makeStyles({
-  label: {
-    marginRight: 0,
-  },
-  position: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-});
-
-export default function CheckBoxHeaderCell({
-  title,
-  checkBoxLabel,
-  checkBoxChangeHandler,
-}: {
-  title: string;
-  checkBoxLabel: string;
-  checkBoxChangeHandler: (event: React.ChangeEvent<HTMLInputElement>) => void;
-}) {
-  const classes = useCheckBoxStyles();
-
-  return (
-    <div className={classes.position}>
-      {title}
-      <FormControlLabel
-        className={classes.label}
-        control={<BlackCheckBox onChange={checkBoxChangeHandler} disabled />}
-        label={checkBoxLabel}
-      />
-    </div>
-  );
-}
-=======
 import React from 'react';
 import { createStyles, FormControlLabel, Theme } from '@material-ui/core';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
@@ -116,4 +61,3 @@ export default function CheckBoxHeaderCell({
     </div>
   );
 }
->>>>>>> addded checkbox to headercell
