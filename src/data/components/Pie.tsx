1a01dcf5d9d38901a46e8c788afc545fe8eb8dff
Alireza Balouch <swape@swape.net>
2020-11-10 14:04:01 +0100

    nivo pie

--- src/data/components/Pie.tsx
+++ src/data/components/Pie.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { ResponsivePie } from '@nivo/pie';
 import {
   PieChart,
   Pie as ChartPie,
@@ -94,6 +95,34 @@ const shapeRenderer = ({ groupKey }: { groupKey: string }) => ({
   );
 };
 
+const colors = ['#a3a1fb', '#56d9fe', '#74e2b7', '#f2efa0'];
+
+export const MyResponsivePie = ({
+  data,
+  groupKey,
+  valueKey,
+}: PieChartsProps) => (
+  <div style={{ height: '300px', width: '100%' }}>
+    <ResponsivePie
+      data={data}
+      value={valueKey}
+      margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
+      innerRadius={0.3}
+      padAngle={0.7}
+      cornerRadius={3}
+      colors={colors}
+      borderWidth={1}
+      borderColor={{ from: 'color', modifiers: [['darker', 0.2]] }}
+      radialLabelsTextColor="#333333"
+      sliceLabelsSkipAngle={10}
+      sliceLabelsTextColor="#333333"
+      id={groupKey}
+      radialLabel={(e) => `${e.label} (${e.value})`}
+      legends={[]}
+    />
+  </div>
+);
+
 export default function Pie({
   data,
   groupKey = 'group',
@@ -101,6 +130,9 @@ export default function Pie({
 }: PieChartsProps) {
   const [activeIndex, setActiveIndex] = useState(0);
 
+  if (true) {
+    return MyResponsivePie({ data, groupKey, valueKey });
+  }
   return (
     <ResponsiveContainer height={280}>
       <PieChart>
