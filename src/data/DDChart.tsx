c5d312a7cbe21e793f9fa1c1c10f96fc7ac80109
fridaklockmann <fridaklockmann@gmail.com>
2020-12-02 11:23:15 +0100

    added big size for fallback on charts

--- src/data/DDChart.tsx
+++ src/data/DDChart.tsx
@@ -20,7 +20,6 @@ const usePlaceholderStyle = makeStyles(() =>
       flexDirection: 'column',
       justifyContent: 'center',
       alignItems: 'center',
-      height: 280,
     },
     titleText: {
       fontSize: '18px',
@@ -34,10 +33,12 @@ const usePlaceholderStyle = makeStyles(() =>
   })
 );
 
-const Placeholder = () => {
+const Placeholder = (props:{big:boolean}) => {
+  
   const classes = usePlaceholderStyle();
+  const height = props.big? '400px': '280px';
   return (
-    <div className={classes.root}>
+    <div className={classes.root} style={{height}}>
       <div className={classes.titleText}>Oida</div>
       <div className={classes.text}>Data kan ikke vises for dette valget</div>
     </div>
