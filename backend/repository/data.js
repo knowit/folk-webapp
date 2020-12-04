f23b144f31b21d88b79ffd1f91b78ff21279aab2
Alireza Balouch <swape@swape.net>
2020-12-04 11:19:21 +0100

    format

--- backend/repository/data.js
+++ backend/repository/data.js
@@ -231,40 +231,39 @@ exports.competenceAreas = async () => {
   };
 };
 
-
 function setInGroups(list) {
   const groupedList = [
-    {years: 'Under 2 år', count:0},
-    {years: '2 til 5 år', count:0},
-    {years: '6 til 10 år', count:0},
-    {years: '11 til 15 år', count:0},
-    {years: '16 til 20 år', count:0},
-    {years: '21 til 25 år', count:0},
-    {years: '26 til 30 år', count:0},
-    {years: 'over 31 år', count:0},
-  ]
+    { years: 'Under 2 år', count: 0 },
+    { years: '2 til 5 år', count: 0 },
+    { years: '6 til 10 år', count: 0 },
+    { years: '11 til 15 år', count: 0 },
+    { years: '16 til 20 år', count: 0 },
+    { years: '21 til 25 år', count: 0 },
+    { years: '26 til 30 år', count: 0 },
+    { years: 'over 31 år', count: 0 },
+  ];
   list.forEach((item) => {
-    const years = Number(item.years)
-    const count = Number(item.count)
-    if (years < 2 ){
-      groupedList[0].count += count
-    }else if (years > 2 && years< 6) {
-      groupedList[1].count += count
-    }else if (years > 5 && years< 11) {
-      groupedList[2].count += count
-    }else if (years > 10 && years< 16) {
-      groupedList[3].count += count
-    }else if (years > 15 && years< 21) {
-      groupedList[4].count += count
-    }else if (years > 20 && years< 26) {
-      groupedList[5].count += count
-    }else if (years > 25 && years< 31) {
-      groupedList[6].count += count
-    }else if (years > 30) {
-      groupedList[7].count += count
+    const years = Number(item.years);
+    const count = Number(item.count);
+    if (years < 2) {
+      groupedList[0].count += count;
+    } else if (years > 2 && years < 6) {
+      groupedList[1].count += count;
+    } else if (years > 5 && years < 11) {
+      groupedList[2].count += count;
+    } else if (years > 10 && years < 16) {
+      groupedList[3].count += count;
+    } else if (years > 15 && years < 21) {
+      groupedList[4].count += count;
+    } else if (years > 20 && years < 26) {
+      groupedList[5].count += count;
+    } else if (years > 25 && years < 31) {
+      groupedList[6].count += count;
+    } else if (years > 30) {
+      groupedList[7].count += count;
     }
   });
-  
+
   return groupedList;
 }
 
@@ -273,7 +272,7 @@ exports.experienceDistribution = async ({ dataplattformClient }) => {
     reportName: 'yearsSinceSchoolDist',
   });
   const experience = await req.json();
-  const experienceGroups = setInGroups(experience)
+  const experienceGroups = setInGroups(experience);
   return {
     componentType: 'Pie',
     setNames: ['Erfaring'],
@@ -324,7 +323,7 @@ exports.education = async ({ dataplattformClient }) => {
     reportName: 'degreeDist',
   });
   const education = await req.json();
-  
+
   return {
     componentType: 'Pie',
     setNames: ['Utdanning'],
