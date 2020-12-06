8ecbd9f6335e467b2b17b092e3af109bcc49ef9b
HeleneSemmerud <63657610+HeleneSemmerud@users.noreply.github.com>
2020-12-06 15:45:14 +0100

    Revert "Adds emails to footer"

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
-
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
@@ -23,11 +22,6 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default function Footer() {
   const classes = useStyles();
-  const DATACORRECTION_EMAIL = process.env.DATACORRECTION_EMAIL
-  const IMPROVEMENTS_EMAIL = process.env.IMPROVEMENTS_EMAIL
-
-  const improvements_hrefString = "mailto:" + IMPROVEMENTS_EMAIL
-  const correction_hrefString = "mailto:" + DATACORRECTION_EMAIL + "?subject=Melding%20om%20datafeil&body=Feilen%20ble%20funnet%20her%3A%20%0D%0AOg%20omhandler%20dette%3A%20"
 
   return (
     <div className={classes.root}>
@@ -35,13 +29,13 @@ export default function Footer() {
         <p>
           Knowit Folk. Lakkegata 53, 0178 Oslo &ensp;|&ensp; Tlf. 02486 &ensp;|
           &ensp;
-          <a className={classes.link} href={improvements_hrefString}>
-            Kontakt og tilbakemeldinger
+          <a className={classes.link} href="mailto:name1@mail.com">
+            Kontakt
           </a>
           &ensp; | &ensp;
           <a
             className={classes.link}
-            href={correction_hrefString}
+            href="mailto:name1@mail.com?subject=Melding%20om%20datafeil&body=Feilen%20ble%20funnet%20her%3A%20%0D%0AOg%20omhandler%20dette%3A%20"
           >
             Meld om datafeil
           </a>
