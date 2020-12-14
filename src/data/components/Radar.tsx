809c8fb351a1070a7b5f8252d745fd76e5f72541
Alireza Balouch <swape@swape.net>
2020-12-14 13:38:36 +0100

    improve the radar chart

--- src/data/components/Radar.tsx
+++ src/data/components/Radar.tsx
@@ -14,10 +14,12 @@ interface RadarChartsProps {
   big?: boolean;
 }
 
+const RadarChartColors = colors.filter((_, k) => k !== 1);
+
 export default function Radar({
   data,
   groupKey = 'kategori',
-  valueKey = [],
+  valueKey = ['motivasjon','kompetanse'],
   big,
 }: RadarChartsProps) {
   const height = big ? '400px' : '300px';
@@ -31,19 +33,17 @@ export default function Radar({
         margin={{ top: 50, right: 10, bottom: 50, left: 10 }}
         curve="linearClosed"
         borderWidth={2}
-        borderColor={colors}
         gridLevels={5}
         gridShape="circular"
-        gridLabelOffset={36}
+        gridLabelOffset={20}
         enableDots={true}
         dotSize={10}
         dotColor={{ theme: 'background' }}
         dotBorderWidth={2}
-        dotBorderColor={colors}
         enableDotLabel={true}
         dotLabel="value"
         dotLabelYOffset={-12}
-        colors={colors}
+        colors={RadarChartColors}
         fillOpacity={0.25}
         blendMode="multiply"
         animate={true}
