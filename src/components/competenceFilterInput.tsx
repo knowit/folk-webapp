5f5a87a5417d3c87b5d499a4497f105db6edf960
fridaklockmann <fridaklockmann@gmail.com>
2020-12-18 16:25:43 +0100

    trying out autocomplete

--- src/components/competenceFilterInput.tsx
+++ src/components/competenceFilterInput.tsx
@@ -2,76 +2,83 @@
 
 import React from 'react';
 import Checkbox from '@material-ui/core/Checkbox';
-import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
 import CheckBoxIcon from '@material-ui/icons/CheckBox';
 import { useFetchedData } from '../hooks/service';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import { makeStyles } from '@material-ui/core';
-import useAutocomplete from '@material-ui/lab/useAutocomplete';
-import NoSsr from '@material-ui/core/NoSsr';
-import CheckIcon from '@material-ui/icons/Check';
-import CloseIcon from '@material-ui/icons/Close';
-import styled from 'styled-components';
+import { makeStyles } from '@material-ui/core/styles';
 
 const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
 const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
-interface CategoryList{
-  category: string,
-  subCategories: string[]
+interface CategoryList {
+  category: string;
+  subCategories: string[];
 }
 type CategoriesWithGroup = {
-  skill:string,
-  category:string
-}
-function useCategories() {
-  const [categories, loading] = useFetchedData<CategoryList[]>({url:"/api/data/competenceFilter"});
-  const categoriesWithGroup:CategoriesWithGroup[] = []
-  categories?.forEach(
-    category => {
-      category.subCategories.forEach(
-        skill =>{
-          categoriesWithGroup.push({
-            skill:skill,
-            category:category.category,
-          });
-        }
-      )
-    }
-  )
-  return [categoriesWithGroup, loading];
-}
+  skill: string;
+  category: string;
+};
 
-
-const useStyles = makeStyles(() => ({
-  root: {
-    height: '43px',
-    width: '260px',
-    margin: '5px',
-    color: 'black',
-    backgroundColor: '#ffffff'
+const useStyles = makeStyles({
+  input: {
+    height: 77,
+    width: 300,
+    backgroundColor:"green",
+    paddingLeft: 12,
+    paddingTop: 0,
+    paddingBottom: 0,
+    lineHeight: '43px',
   },
-}));
+});
 
 
+function useCategories() {
+  const [categories] = useFetchedData<CategoryList[]>({
+    url: '/api/data/competenceFilter',
+  });
+  const categoriesWithGroup: CategoriesWithGroup[] = [];
+  categories?.forEach((category) => {
+    category.subCategories.forEach((skill) => {
+      categoriesWithGroup.push({
+        skill: skill,
+        category: category.category,
+      });
+    });
+  });
+  if (categoriesWithGroup !== null) {
+    return categoriesWithGroup;
+  } else {
+    return [{ skill: '', category: '' }];
+  }
+}
 
-export function CheckboxesTags() {
+export default function CheckboxesTags() {
+  const categoriesWithGroup = useCategories();
+  const filterList:string[] = [];
+  const alterFilterList = (skill:string, selected:boolean) => {
+    if(selected === true){
+      filterList.push(skill)
+    }else{
+      const index = filterList.indexOf(skill, 0);
+      if (index > -1) {
+        filterList.splice(index, 1);
+      }
+    }
+    console.log(filterList)
+  }
   const classes = useStyles();
-  const [categoriesWithGroup, loading] = useCategories();
   return (
     <Autocomplete
+      style={{ width: 300 }}
       multiple
-      className = {classes.root}
       autoComplete
       autoHighlight
       autoSelect
       id="kompetansefilter"
-      options={categoriesWithGroup || [{skill:"loading", category:""}]}
-      loading={loading}
+      options={categoriesWithGroup}
       disableCloseOnSelect
-      groupBy ={(option) => option.category}
+      groupBy={(option) => option.category}
       getOptionLabel={(options) => options.skill}
       renderOption={(options, { selected }) => (
         <React.Fragment>
@@ -80,433 +87,17 @@ export function CheckboxesTags() {
             checkedIcon={checkedIcon}
             style={{ marginRight: 8 }}
             checked={selected}
+            onClick = {() => alterFilterList(options.skill, !selected)}
           />
           {options.skill}
-          {loading ? <CircularProgress color="inherit" size={20} /> : null}
         </React.Fragment>
       )}
       renderInput={(params) => (
-        <TextField
-          {...params}
-          variant="outlined"
-          label="Filtrer"
-          placeholder="Kompetanseområder"
-        />
+        <div ref={params.InputProps.ref}>
+          {console.log(params.inputProps)}
+          <input className={classes.input} type="text" {...params.inputProps} placeholder="filtrer på kompetanse.."/>
+        </div>
       )}
     />
-  );
-}
-
-const Label = styled('label')`
-  padding: 0 0 4px;
-  line-height: 1.5;
-  display: block;
-`;
-
-const InputWrapper = styled('div')`
-  width: 300px;
-  border: 1px solid #d9d9d9;
-  background-color: #fff;
-  border-radius: 4px;
-  padding: 1px;
-  display: flex;
-  flex-wrap: wrap;
-
-  &:hover {
-    border-color: #40a9ff;
-  }
-
-  &.focused {
-    border-color: #40a9ff;
-    box-shadow: 0 0 0 2px rgba(24, 144, 255, 0.2);
-  }
-
-  & input {
-    font-size: 14px;
-    height: 30px;
-    box-sizing: border-box;
-    padding: 4px 6px;
-    width: 0;
-    min-width: 30px;
-    flex-grow: 1;
-    border: 0;
-    margin: 0;
-    outline: 0;
-  }
-`;
-
-const Tag = styled(({ label, onDelete, ...props }) => (
-  <div {...props}>
-    <span>{label}</span>
-    <CloseIcon onClick={onDelete} />
-  </div>
-))`
-  display: flex;
-  align-items: center;
-  height: 24px;
-  margin: 2px;
-  line-height: 22px;
-  background-color: #fafafa;
-  border: 1px solid #e8e8e8;
-  border-radius: 2px;
-  box-sizing: content-box;
-  padding: 0 4px 0 10px;
-  outline: 0;
-  overflow: hidden;
-
-  &:focus {
-    border-color: #40a9ff;
-    background-color: #e6f7ff;
-  }
-
-  & span {
-    overflow: hidden;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-  }
-
-  & svg {
-    font-size: 12px;
-    cursor: pointer;
-    padding: 4px;
-  }
-`;
-
-const Listbox = styled('ul')`
-  width: 300px;
-  margin: 2px 0 0;
-  padding: 0;
-  position: absolute;
-  list-style: none;
-  background-color: #fff;
-  overflow: auto;
-  max-height: 250px;
-  border-radius: 4px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);
-  z-index: 1;
-
-  & li {
-    padding: 5px 12px;
-    display: flex;
-
-    & span {
-      flex-grow: 1;
-    }
-
-    & svg {
-      color: transparent;
-    }
-  }
-
-  & li[aria-selected='true'] {
-    background-color: #fafafa;
-    font-weight: 600;
-
-    & svg {
-      color: #1890ff;
-    }
-  }
-
-  & li[data-focus='true'] {
-    background-color: #e6f7ff;
-    cursor: pointer;
-
-    & svg {
-      color: #000;
-    }
-  }
-`;
-
-export default function CustomizedHook() {
-  const [categoriesWithGroup, loading] = useCategories();
-  const defaultValue = categoriesWithGroup || [{skill:"loading", category:""}]
-  const {
-    getRootProps,
-    getInputLabelProps,
-    getInputProps,
-    getTagProps,
-    getListboxProps,
-    getOptionProps,
-    groupedOptions,
-    value,
-    focused,
-    setAnchorEl,
-  } = useAutocomplete({
-    id: 'customizedAutocomplete',
-    defaultValue: defaultValue,
-    multiple: true,
-    options: categoriesWithGroup,
-    getOptionLabel: (option) => option.skill,
-    autoComplete:true,
-    autoHighlight:true,
-    autoSelect:true,
-    loading:loading,
-    groupBy: (option) => option.category,
-  });
-
-  return (
-    <NoSsr>
-      <div>
-        <div {...getRootProps()}>
-          <Label {...getInputLabelProps()}>Customized hook</Label>
-          <InputWrapper ref={setAnchorEl} className={focused ? 'focused' : ''}>
-            {value.map((option: CategoriesWithGroup, index: number) => (
-              <Tag label={option.skill} {...getTagProps({ index })} />
-            ))}
-            <input {...getInputProps()} />
-          </InputWrapper>
-        </div>
-        {groupedOptions.length > 0 ? (
-          <Listbox {...getListboxProps()}>
-            {groupedOptions.map((option, index) => (
-              <li {...getOptionProps({ option, index })}>
-                <span>{option.skill}</span>
-                <CheckIcon fontSize="small" />
-              </li>
-            ))}
-          </Listbox>
-        ) : null}
-      </div>
-    </NoSsr>
-  );
-}
-
-
-
-/* eslint-disable no-use-before-define */
-import React from 'react';
-import { useTheme, fade, makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import Popper from '@material-ui/core/Popper';
-import SettingsIcon from '@material-ui/icons/Settings';
-import CloseIcon from '@material-ui/icons/Close';
-import DoneIcon from '@material-ui/icons/Done';
-import Autocomplete, { AutocompleteCloseReason } from '@material-ui/lab/Autocomplete';
-import ButtonBase from '@material-ui/core/ButtonBase';
-import InputBase from '@material-ui/core/InputBase';
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      width: 221,
-      fontSize: 13,
-    },
-    button: {
-      fontSize: 13,
-      width: '100%',
-      textAlign: 'left',
-      paddingBottom: 8,
-      color: '#586069',
-      fontWeight: 600,
-      '&:hover,&:focus': {
-        color: '#0366d6',
-      },
-      '& span': {
-        width: '100%',
-      },
-      '& svg': {
-        width: 16,
-        height: 16,
-      },
-    },
-    tag: {
-      marginTop: 3,
-      height: 20,
-      padding: '.15em 4px',
-      fontWeight: 600,
-      lineHeight: '15px',
-      borderRadius: 2,
-    },
-    popper: {
-      border: '1px solid rgba(27,31,35,.15)',
-      boxShadow: '0 3px 12px rgba(27,31,35,.15)',
-      borderRadius: 3,
-      width: 300,
-      zIndex: 1,
-      fontSize: 13,
-      color: '#586069',
-      backgroundColor: '#f6f8fa',
-    },
-    header: {
-      borderBottom: '1px solid #e1e4e8',
-      padding: '8px 10px',
-      fontWeight: 600,
-    },
-    inputBase: {
-      padding: 10,
-      width: '100%',
-      borderBottom: '1px solid #dfe2e5',
-      '& input': {
-        borderRadius: 4,
-        backgroundColor: theme.palette.common.white,
-        padding: 8,
-        transition: theme.transitions.create(['border-color', 'box-shadow']),
-        border: '1px solid #ced4da',
-        fontSize: 14,
-        '&:focus': {
-          boxShadow: `${fade(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
-          borderColor: theme.palette.primary.main,
-        },
-      },
-    },
-    paper: {
-      boxShadow: 'none',
-      margin: 0,
-      color: '#586069',
-      fontSize: 13,
-    },
-    option: {
-      minHeight: 'auto',
-      alignItems: 'flex-start',
-      padding: 8,
-      '&[aria-selected="true"]': {
-        backgroundColor: 'transparent',
-      },
-      '&[data-focus="true"]': {
-        backgroundColor: theme.palette.action.hover,
-      },
-    },
-    popperDisablePortal: {
-      position: 'relative',
-    },
-    iconSelected: {
-      width: 17,
-      height: 17,
-      marginRight: 5,
-      marginLeft: -2,
-    },
-    color: {
-      width: 14,
-      height: 14,
-      flexShrink: 0,
-      borderRadius: 3,
-      marginRight: 8,
-      marginTop: 2,
-    },
-    text: {
-      flexGrow: 1,
-    },
-    close: {
-      opacity: 0.6,
-      width: 18,
-      height: 18,
-    },
-  }),
-);
-
-export function GitHubLabel() {
-  const classes = useStyles();
-  const [categoriesWithGroup, loading] = useCategories();
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const [value, setValue] = React.useState<CategoriesWithGroup[]>(categoriesWithGroup);
-  const [pendingValue, setPendingValue] = React.useState<CategoriesWithGroup[]>([]);
-  const theme = useTheme();
-
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setPendingValue(value);
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = (event: React.ChangeEvent<{}>, reason: AutocompleteCloseReason) => {
-    if (reason === 'toggleInput') {
-      return;
-    }
-    setValue(pendingValue);
-    if (anchorEl) {
-      anchorEl.focus();
-    }
-    setAnchorEl(null);
-  };
-
-  const open = Boolean(anchorEl);
-  const id = open ? 'github-label' : undefined;
-
-  return (
-    <React.Fragment>
-      <div className={classes.root}>
-        <ButtonBase
-          disableRipple
-          className={classes.button}
-          aria-describedby={id}
-          onClick={handleClick}
-        >
-          <span>Labels</span>
-          <SettingsIcon />
-        </ButtonBase>
-        {value.map((label) => (
-          <div
-            key={label.name}
-            className={classes.tag}
-            style={{
-              backgroundColor: label.color,
-              color: theme.palette.getContrastText(label.color),
-            }}
-          >
-            {label.name}
-          </div>
-        ))}
-      </div>
-      <Popper
-        id={id}
-        open={open}
-        anchorEl={anchorEl}
-        placement="bottom-start"
-        className={classes.popper}
-      >
-        <div className={classes.header}>Apply labels to this pull request</div>
-        <Autocomplete
-          open
-          onClose={handleClose}
-          multiple
-          classes={{
-            paper: classes.paper,
-            option: classes.option,
-            popperDisablePortal: classes.popperDisablePortal,
-          }}
-          value={pendingValue}
-          onChange={(event, newValue) => {
-            setPendingValue(newValue);
-          }}
-          disableCloseOnSelect
-          disablePortal
-          renderTags={() => null}
-          noOptionsText="No labels"
-          renderOption={(option, { selected }) => (
-            <React.Fragment>
-              <DoneIcon
-                className={classes.iconSelected}
-                style={{ visibility: selected ? 'visible' : 'hidden' }}
-              />
-              <span className={classes.color} style={{ backgroundColor: option.color }} />
-              <div className={classes.text}>
-                {option.name}
-                <br />
-                {option.description}
-              </div>
-              <CloseIcon
-                className={classes.close}
-                style={{ visibility: selected ? 'visible' : 'hidden' }}
-              />
-            </React.Fragment>
-          )}
-          options={[...labels].sort((a, b) => {
-            // Display the selected labels first.
-            let ai = value.indexOf(a);
-            ai = ai === -1 ? value.length + labels.indexOf(a) : ai;
-            let bi = value.indexOf(b);
-            bi = bi === -1 ? value.length + labels.indexOf(b) : bi;
-            return ai - bi;
-          })}
-          getOptionLabel={(option) => option.name}
-          renderInput={(params) => (
-            <InputBase
-              ref={params.InputProps.ref}
-              inputProps={params.inputProps}
-              autoFocus
-              className={classes.inputBase}
-            />
-          )}
-        />
-      </Popper>
-    </React.Fragment>
   );
 }
