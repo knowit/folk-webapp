804a46ef720562d84fcd94439ea9b86ec5d0801a
fridaklockmann <fridaklockmann@gmail.com>
2021-01-05 09:09:04 +0100

    uses dispatch instead of state

--- src/data/DDTable.tsx
+++ src/data/DDTable.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import { GridItemHeader } from '../components/GridItem';
 import { FilterHeader } from '../components/FilterHeader';
 import DataTable from './components/table/DataTable';
 import SearchInput from '../components/SearchInput';
-import CompetenceFilterInput from  '../components/CompetenceFilterInput'
+import CompetenceFilterInput from '../components/CompetenceFilterInput';
 import { DDComponentProps } from './types';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -20,14 +20,220 @@ const useStyles = makeStyles({
     display: 'flex',
     justifyContent: 'space-around',
     width: '600px',
-  }
+  },
 });
+
+export interface RowStates {
+  rows: any[];
+  motivationFilter: string[];
+  competenceFilter: string[];
+  searchTerm: string;
+}
+
+export type Action =
+  | {
+      type: 'ADD_TO_MOTIVATION_FILTER';
+      filter: string;
+      allRows: any[];
+      searchableColumns: any[];
+    }
+  | {
+      type: 'REMOVE_FROM_MOTIVATION_FILTER';
+      filter: string;
+      allRows: any[];
+      searchableColumns: any[];
+    }
+  | {
+      type: 'CLEAR_MOTIVATION_FILTER';
+      allRows: any[];
+      searchableColumns: any[];
+    }
+  | {
+      type: 'ADD_TO_COMPETENCE_FILTER';
+      filter: string;
+      allRows: any[];
+      searchableColumns: any[];
+    }
+  | {
+      type: 'REMOVE_FROM_COMPETENCE_FILTER';
+      filter: string;
+      allRows: any[];
+      searchableColumns: any[];
+    }
+  | {
+      type: 'CLEAR_COMPETENCE_FILTER';
+      allRows: any[];
+      searchableColumns: any[];
+    }
+  | {
+      type: 'CHANGE_SEARCH_TERM';
+      searchTerm: string;
+      allRows: any[];
+      searchableColumns: any[];
+    };
+
+function reducer(currentState: RowStates, action: Action) {
+  switch (action.type) {
+    case 'ADD_TO_MOTIVATION_FILTER':
+      return {
+        rows: searchAndFilter(
+          currentState.rows,
+          [...currentState.motivationFilter, action.filter],
+          currentState.competenceFilter,
+          currentState.searchTerm,
+          action.allRows,
+          action.searchableColumns
+        ),
+        motivationFilter: [...currentState.motivationFilter, action.filter],
+        competenceFilter: currentState.competenceFilter,
+        searchTerm: currentState.searchTerm,
+      };
+    case 'REMOVE_FROM_MOTIVATION_FILTER':
+      return {
+        rows: searchAndFilter(
+          currentState.rows,
+          currentState.motivationFilter.filter(
+            (filter) => filter !== action.filter
+          ),
+          currentState.competenceFilter,
+          currentState.searchTerm,
+          action.allRows,
+          action.searchableColumns
+        ),
+        motivationFilter: currentState.motivationFilter.filter(
+          (filter) => filter !== action.filter
+        ),
+        competenceFilter: currentState.competenceFilter,
+        searchTerm: currentState.searchTerm,
+      };
+    case 'CLEAR_MOTIVATION_FILTER':
+      return {
+        rows: searchAndFilter(
+          currentState.rows,
+          [],
+          currentState.competenceFilter,
+          currentState.searchTerm,
+          action.allRows,
+          action.searchableColumns
+        ),
+        motivationFilter: [],
+        competenceFilter: currentState.competenceFilter,
+        searchTerm: currentState.searchTerm,
+      };
+    case 'ADD_TO_COMPETENCE_FILTER':
+      return {
+        rows: searchAndFilter(
+          currentState.rows,
+          currentState.motivationFilter,
+          [...currentState.competenceFilter, action.filter],
+          currentState.searchTerm,
+          action.allRows,
+          action.searchableColumns
+        ),
+        motivationFilter: currentState.motivationFilter,
+        competenceFilter: [...currentState.competenceFilter, action.filter],
+        searchTerm: currentState.searchTerm,
+      };
+    case 'REMOVE_FROM_COMPETENCE_FILTER':
+      return {
+        rows: searchAndFilter(
+          currentState.rows,
+          currentState.motivationFilter,
+          currentState.competenceFilter.filter(
+            (filter) => filter !== action.filter
+          ),
+          currentState.searchTerm,
+          action.allRows,
+          action.searchableColumns
+        ),
+        motivationFilter: currentState.motivationFilter,
+        competenceFilter: currentState.competenceFilter.filter(
+          (filter) => filter !== action.filter
+        ),
+        searchTerm: currentState.searchTerm,
+      };
+    case 'CLEAR_COMPETENCE_FILTER':
+      return {
+        rows: searchAndFilter(
+          currentState.rows,
+          currentState.motivationFilter,
+          [],
+          currentState.searchTerm,
+          action.allRows,
+          action.searchableColumns
+        ),
+        motivationFilter: currentState.motivationFilter,
+        competenceFilter: [],
+        searchTerm: currentState.searchTerm,
+      };
+    case 'CHANGE_SEARCH_TERM':
+      return {
+        rows: currentState.rows,
+        motivationFilter: currentState.motivationFilter,
+        competenceFilter: currentState.competenceFilter,
+        searchTerm: action.searchTerm,
+      };
+    default:
+      return currentState;
+  }
+}
+
+const searchAndFilter = (
+  rows: any[],
+  motivationFilter: string[],
+  competenceFilter: string[],
+  searchTerm: string,
+  allRows: any[],
+  searchableColumn: any[]
+) => {
+  const filterLength = motivationFilter.length;
+  if ((!searchTerm || searchTerm.length <= 2) && filterLength === 0) {
+    if (rows.length !== allRows.length) return allRows;
+    return rows;
+  }
+  const lowerCaseSearchTerm = searchTerm?.toLowerCase();
+
+  //searching for name
+  const searchedRows = allRows.filter((row) => {
+    return searchableColumn
+      .map(([key, index]) => {
+        const rowValue = key
+          ? row.rowData[index][key].toLowerCase()
+          : row.rowData[index].toLowerCase();
+        return rowValue.includes(lowerCaseSearchTerm);
+      })
+      .reduce((a, b) => a || b, false);
+  });
+
+  //if no filter, then skip filtering
+  if (filterLength === 0) {
+    return searchedRows;
+  }
+
+  const newRows: any[] = [];
+
+  searchedRows.forEach((row) => {
+    var passedFilters = 0;
+    motivationFilter.forEach((skill) => {
+      const rowSkills = row.rowData[row.rowData.length - 1];
+      if (rowSkills?.indexOf(skill) !== -1 && rowSkills !== undefined) {
+        passedFilters++;
+      }
+    });
+    passedFilters === filterLength && newRows.push(row);
+  });
+  return newRows;
+};
 
 export default function DDTable({ payload, title, props }: DDComponentProps) {
   const allRows = payload as { rowData: any[] }[];
-  const [rows, setRows] = useState(allRows);
-  const [filterList, setFilterList] = useState<string[]>([]);
-  const [searchBarInput, setSearchBarInput] = useState<string>();
+  const initialState: RowStates = {
+    rows: allRows,
+    motivationFilter: [],
+    competenceFilter: [],
+    searchTerm: '',
+  };
+  const [state, dispatch] = useReducer(reducer, initialState);
   const { columns } = props as { columns: Column[] };
   const searchableColumn = columns
     .map(
@@ -37,65 +243,34 @@ export default function DDTable({ payload, title, props }: DDComponentProps) {
     .filter(([searchable]) => searchable)
     .map(([, key, i]) => [key, i] as [string, number]);
 
-  const searchAndFilter = () => {
-    const filterLength = filterList.length
-    if ((!searchBarInput || searchBarInput.length <= 2) && filterLength === 0) {
-      if (rows.length !== allRows.length) setRows(allRows);
-      return;
-    }
-    const lowerCaseSearchTerm = searchBarInput?.toLowerCase();
-
-    //searching for name 
-    const searchedRows = allRows.filter((row) => {
-        return searchableColumn
-          .map(([key, index]) => {
-            const rowValue = key
-              ? row.rowData[index][key].toLowerCase()
-              : row.rowData[index].toLowerCase();
-            return rowValue.includes(lowerCaseSearchTerm);
-          })
-          .reduce((a, b) => a || b, false);
-      })
-    
-    //if no filter, then skip filtering
-    if (filterLength === 0 ){
-      setRows(searchedRows)
-      return;
-    } 
-
-    const newRows:any[] = []
-    
-    searchedRows.forEach(row=>{
-      var passedFilters = 0
-      filterList.forEach(skill =>{
-        const rowSkills = row.rowData[row.rowData.length-1]
-        if(rowSkills?.indexOf(skill)!== -1 && rowSkills !== undefined){
-          passedFilters++; 
-        }
-      })
-      passedFilters === filterLength && newRows.push(row)
-    })
-    setRows(newRows)
-  }
-
-  useEffect(()=>{
-    searchAndFilter();
-  },[filterList.length, searchBarInput])
-
-
-
   const classes = useStyles();
 
   return (
     <>
       <GridItemHeader title={title}>
         <div className={classes.searchBars}>
-          <CompetenceFilterInput filterList={filterList} setFilterList={setFilterList}/>
-          <SearchInput onChange={setSearchBarInput} />
+          <CompetenceFilterInput
+            filterList={state.motivationFilter}
+            dispatch={dispatch}
+            allRows={allRows}
+            searchableColumns={searchableColumn}
+          />
+          <SearchInput
+            dispatch={dispatch}
+            allRows={allRows}
+            searchableColumns={searchableColumn}
+          />
         </div>
       </GridItemHeader>
-      {filterList.length > 0 && <FilterHeader  filterList={filterList} setFilterList={setFilterList} />}
-      <DataTable rows={rows} columns={[]} {...props} />
+      {state.motivationFilter.length > 0 && (
+        <FilterHeader
+          filterList={state.motivationFilter}
+          dispatch={dispatch}
+          allRows={allRows}
+          searchableColumns={searchableColumn}
+        />
+      )}
+      <DataTable rows={state.rows} columns={[]} {...props} />
     </>
   );
 }
