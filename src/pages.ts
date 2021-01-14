25439b61b6d421b1387a03f6b0f5f3df095bd9f6
fridaklockmann <fridaklockmann@gmail.com>
2021-01-14 10:21:01 +0100

    added route to site

--- src/pages.ts
+++ src/pages.ts
@@ -2,5 +2,6 @@ import NotFound from './pages/NotFound';
 import UnderConstruction from './pages/UnderConstruction';
 import Employee from './pages/Employee';
 import Competence from './pages/Competence';
+import EmployeeSite from './pages/EmployeeSite';
 
-export { NotFound, UnderConstruction, Employee, Competence };
+export { NotFound, UnderConstruction, Employee, Competence, EmployeeSite };
