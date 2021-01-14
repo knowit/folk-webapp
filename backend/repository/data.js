c43e98af760d9dbc688e6cfd55165389f3be5935
Vebjørn Hansen Øvsthus <vebovs@hotmail.no>
2021-01-14 09:33:24 +0100

    minor cleanup of daterange

--- backend/repository/data.js
+++ backend/repository/data.js
@@ -502,23 +502,24 @@ const convertToNum = (dates) => {
   return data;
 }
 
-//Needs refactoring
 const dateRange = (startDate, endDate) => {
-  var start      = startDate.split('-');
-  var end        = endDate.split('-');
-  var startYear  = parseInt(start[0]);
-  var endYear    = parseInt(end[0]);
-  var dates      = [];
 
-  for(var i = startYear; i <= endYear; i++) {
-    var endMonth = i != endYear ? 11 : parseInt(end[1]) - 1;
-    var startMon = i === startYear ? parseInt(start[1])-1 : 0;
-    for(var j = startMon; j <= endMonth; j = j > 12 ? j % 12 || 11 : j+1) {
-      var month = j+1;
-      var displayMonth = month < 10 ? '0'+month : month;
+  const start = startDate.split('-');
+  const end = endDate.split('-');
+  const startYear = parseInt(start[0]);
+  const endYear = parseInt(end[0]);
+  const dates = [];
+
+  for(let i = startYear; i <= endYear; i++) {
+    const endMonth = i != endYear ? 11 : parseInt(end[1]) - 1;
+    const startMon = i === startYear ? parseInt(start[1]) - 1 : 0;
+    for(let j = startMon; j <= endMonth; j = j > 12 ? j % 12 || 11 : j + 1) {
+      const month = j + 1;
+      const displayMonth = month < 10 ? '0' + month : month;
       dates.push([i, displayMonth, '01'].join('-'));
     }
   }
+
   return dates;
 }
 
