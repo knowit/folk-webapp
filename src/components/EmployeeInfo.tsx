6db27b43474555f12079e303516c0eccdb265f6b
fridaklockmann <fridaklockmann@gmail.com>
2021-01-14 11:07:51 +0100

    moved type

--- src/components/EmployeeInfo.tsx
+++ src/components/EmployeeInfo.tsx
@@ -13,7 +13,7 @@ type Experience = {
   month_to: number;
   year_to: number;
 };
-
+type Date = { year: number; month: number } | { year: number };
 type MotivationMap = {
   [category: string]: number;
 };
@@ -84,14 +84,12 @@ export const startedInKnowit = (allExperience: Experience[] | undefined) => {
 };
 
 export const totalExperience = (allExperience: Experience[] | undefined) => {
-  const dates:Date[] = [];
-  allExperience?.map(job =>{
-    job.year_from !== -1 && dates.push({year:job.year_from})
-    job.year_to !== -1 && dates.push({year:job.year_to})
-  })
-  const firstJob = dates?.sort(
-    (a, b) => a.year - b.year
-  )[0];
+  const dates: Date[] = [];
+  allExperience?.map((job) => {
+    job.year_from !== -1 && dates.push({ year: job.year_from });
+    job.year_to !== -1 && dates.push({ year: job.year_to });
+  });
+  const firstJob = dates?.sort((a, b) => a.year - b.year)[0];
 
   return firstJob?.year === undefined || firstJob?.year < 0 ? (
     <NoData />
@@ -192,9 +190,6 @@ export default function EmployeeInfo({
   const classes = useStyles();
   const url = data.competenceUrl;
   const [empData, pending] = useFetchedData<EmployeeInfoData>({ url });
-  
-  type Date = {year:number, month:number} | {year:number}
-
 
   const getStringFromList = (
     list: string[] | null | undefined,
