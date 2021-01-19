0a2fd178a46c8487298a47569920a61e302ba0db
fridaklockmann <fridaklockmann@gmail.com>
2021-01-19 16:29:34 +0100

    displaying data from reports

--- src/pages/EmployeeSite.tsx
+++ src/pages/EmployeeSite.tsx
@@ -5,6 +5,8 @@ import { Skeleton } from '@material-ui/lab';
 import DDItem, { DDChart } from '../data/DDItem';
 import { useFetchedData } from '../hooks/service';
 import { startedInKnowit, totalExperience } from '../components/EmployeeInfo';
+import { months } from '../data/components/table/cells/ExperienceCell';
+import { NoData } from '../components/ErrorText';
 
 type Experience = {
   employer: string;
@@ -19,16 +21,23 @@ type EmpData = {
   title: string;
   user_id: string;
 };
-type TagData = {
-  skill: string;
-  role: string;
-  language: string;
-};
+
 type EmpSiteData = {
   id: string;
   employee: EmpData;
-  tags: TagData;
+  tags: {
+    skill: string;
+    role: string;
+    language: string;
+  };
   workExperience: Experience[];
+  degree:string; 
+  links:  {
+    no_pdf: string,
+    int_pdf: string,
+    no_word: string,
+    int_word: string,
+}
 };
 
 const useStyles = makeStyles({
@@ -47,8 +56,25 @@ const useStyles = makeStyles({
 });
 
 const ChartSkeleton = () => (
-  <Skeleton variant="rect" height={320} animation="wave" />
+  <Skeleton variant="rect" height={320} width={400} animation="wave" />
 );
+
+function printWorkExperience(workExperience:Experience[]|undefined|null){
+  if(!workExperience) return <NoData/>
+  //sort so newest is first
+  workExperience.sort((a, b) => b.year_from - a.year_from);
+  const getYear = (year:number) =>(
+    year!== -1 ? year : null
+  ) 
+  return workExperience.map((exp, index) => (
+    <div key={index}>
+      <h4>
+        {months[exp.month_from]} {getYear(exp.year_from)} - {months[exp.month_to]} {getYear(exp.year_to)}
+      </h4>
+      <div>{exp.employer}</div>
+    </div>
+  ));
+}
 
 export default function EmployeeSite() {
   const location = useLocation();
@@ -130,6 +156,16 @@ export default function EmployeeSite() {
             <Skeleton variant="rect" width={340} height={15} animation="wave" />
           ) : (
             <>
+              <b>Utdanning: </b>
+              {data?.degree}
+            </>
+          )}
+        </div>
+        <div className={classes.cell}>
+          {pending ? (
+            <Skeleton variant="rect" width={340} height={15} animation="wave" />
+          ) : (
+            <>
               <b>Nærmeste leder: </b>
               *DATA FRA AD HER*
             </>
@@ -150,6 +186,22 @@ export default function EmployeeSite() {
             />
           )}
         </Grid>
+        <div>
+          <h1>Arbeidserfaring</h1>
+          {pending ? (
+            <p>loading....</p>
+          ) : ( printWorkExperience(data?.workExperience)
+          )}
+        </div>
+        <div>
+          <h1>Download CV</h1>
+          {pending || !data ? (
+            <p>loading....</p>
+          ) : ( Object.entries(data!.links).map(link=>(
+            <p key={link[1]} ><a  href={link[1]}>{link[0]}</a></p>
+          ))
+          )}
+        </div>
       </div>
     </>
   );
