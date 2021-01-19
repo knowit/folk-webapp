8cb3e1c02de9c4f4a3dc598730d5e3c40e3374ec
Aleksander Azizi <aleksander.azizi@knowit.no>
2021-01-19 10:28:02 +0100

    Removed legacy AWS solution for running locally

--- backend/repository/util.js
+++ backend/repository/util.js
@@ -1,15 +1,6 @@
 const crypto = require('crypto');
 const AWS = require('aws-sdk');
 
-// PS: this code is only for local dev. Do not ship to dev/prod
-// if (process && process.env && process.env.AWS_ACCESS_KEY_ID) {
-//   AWS.config.update({
-//     region: 'eu-central-1',
-//     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-//   });
-// }
-
 const ssm = new AWS.SSM();
 
 exports.getSecret = (name, { encrypted = false } = {}) => {
