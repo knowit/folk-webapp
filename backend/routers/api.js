34b58b5ad6399204b16fcd1b7a1591b1a689f163
Aleksander Azizi <aleksander.azizi@knowit.no>
2021-02-09 16:09:20 +0100

    Cleanup

--- backend/routers/api.js
+++ backend/routers/api.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.use(dataplattform());
 router.get('/data/:source', async (req, res) => {
   const response = await handler(req);
-  // console.log("api response", response)
+  //console.log("api response", response)
   res.send(response);
 });
 
