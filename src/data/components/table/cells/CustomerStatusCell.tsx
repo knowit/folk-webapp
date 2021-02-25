d1421f00d56035225596253af64fc26efc704bbc
fridaklockmann <fridaklockmann@gmail.com>
2021-02-25 15:58:53 +0100

    adjusted customer cell to display the data

--- src/data/components/table/cells/CustomerStatusCell.tsx
+++ src/data/components/table/cells/CustomerStatusCell.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import CharacterLimitBox from '../../../../components/CharacterLimitBox';
 
-
-interface CustomerStatusData {
-  data: { value: string; };
-  rowData: any[];
+export interface CustomerStatusData {
+  customer: string, 
+  workOrderDescription: string, 
+  weight:number;
 }
 
 const useStyles = makeStyles({
@@ -16,11 +16,18 @@ const useStyles = makeStyles({
   },
 });
 
-export default function CustomerStatusCell(customerData: CustomerStatusData) {
+export default function CustomerStatusCell(customerData:{data: CustomerStatusData[]}) {
   const classes = useStyles();
+  if (customerData.data[0].customer){
+    const mostWorked = customerData.data.sort((customerA, customerB) => customerA.weight - customerB.weight)[0];
+    return(
+    <div className={classes.root}>
+      {mostWorked.customer}: {mostWorked.workOrderDescription}
+    </div>)
+  }
   return (
     <div className={classes.root}>
-      <CharacterLimitBox text={customerData.data.value || '-'} />
+      <CharacterLimitBox text={"Ikke i prosjekt"} />
     </div>
-  );
-}
+  )
+  }
