fa2f08ad443af7f4a052b312688a76e6d74ca391
fridaklockmann <fridaklockmann@gmail.com>
2021-02-25 16:09:31 +0100

    formated and removed characterLimitBox

--- src/data/components/table/cells/CustomerStatusCell.tsx
+++ src/data/components/table/cells/CustomerStatusCell.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import CharacterLimitBox from '../../../../components/CharacterLimitBox';
 
 export interface CustomerStatusData {
-  customer: string, 
-  workOrderDescription: string, 
-  weight:number;
+  customer: string;
+  workOrderDescription: string;
+  weight: number;
 }
 
 const useStyles = makeStyles({
@@ -16,18 +15,19 @@ const useStyles = makeStyles({
   },
 });
 
-export default function CustomerStatusCell(customerData:{data: CustomerStatusData[]}) {
+export default function CustomerStatusCell(customerData: {
+  data: CustomerStatusData[];
+}) {
   const classes = useStyles();
-  if (customerData.data[0].customer){
-    const mostWorked = customerData.data.sort((customerA, customerB) => customerA.weight - customerB.weight)[0];
-    return(
-    <div className={classes.root}>
-      {mostWorked.customer}: {mostWorked.workOrderDescription}
-    </div>)
+  if (customerData.data[0].customer) {
+    const mostWorked = customerData.data.sort(
+      (customerA, customerB) => customerA.weight - customerB.weight
+    )[0];
+    return (
+      <div className={classes.root}>
+        {mostWorked.customer}: {mostWorked.workOrderDescription}
+      </div>
+    );
   }
-  return (
-    <div className={classes.root}>
-      <CharacterLimitBox text={"Ikke i prosjekt"} />
-    </div>
-  )
-  }
+  return <div className={classes.root}>Ikke i prosjekt</div>;
+}
