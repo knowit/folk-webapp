0c211ff06d596f8df1e370816fca34962ef59932
fridaklockmann <fridaklockmann@gmail.com>
2021-03-01 12:52:31 +0100

    formated

--- backend/repository/util.js
+++ backend/repository/util.js
@@ -1,6 +1,6 @@
 const crypto = require('crypto')
 const AWS = require('aws-sdk')
-AWS.config.update({region: 'eu-central-1'})
+AWS.config.update({ region: 'eu-central-1' })
 
 const ssm = new AWS.SSM()
 
