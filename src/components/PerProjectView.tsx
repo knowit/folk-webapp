20bda0f432b944f67963da861196fd4512e0da6c
Sigurd Øines <sigurd.oines@knowit.no>
2021-08-31 10:32:26 +0200

    Small tweaks to Customer page bar chart

--- src/components/PerProjectView.tsx
+++ src/components/PerProjectView.tsx
@@ -3,7 +3,7 @@ import { useFetchedData } from '../hooks/service';
 import type { DDPayload } from '../data/types';
 import { Skeleton } from '@material-ui/lab';
 import { DDChart } from '../data/DDItem';
-import { makeStyles } from '@material-ui/core';
+import { makeStyles, Paper } from '@material-ui/core';
 
 
 export const ChartSkeleton = () => (
@@ -27,33 +27,40 @@ export function PerProjectView() {
       <div>Oops, noe gikk galt!</div>
     </>
   ) : (
-    <>
-      <div className={classes.root}>
-        {pending || !payload ? (
-            <ChartSkeleton />
-          ) : (
-            <DDChart
-              payload={{
-                setNames: Object.keys(payload),
-                sets: payload
-              }}
-              title="Antall ansatte og timer per kunde"
-              props={{
-                chartVariants: [
-                    {
-                      type: 'Bar',
-                      props: {
-                        dataKey: 'customer',
-                        yLabels: ['value'],
+      <Paper
+        style={{
+          height: 500,
+          margin: '2em',
+          width: '100%',
+          backgroundColor: 'white',
+        }}
+      >
+        <div className={classes.root}>
+          {pending || !payload ? (
+              <ChartSkeleton />
+            ) : (
+              <DDChart
+                payload={{
+                  setNames: Object.keys(payload),
+                  sets: payload
+                }}
+                title="Antall ansatte og timer fra siste UBW-periode"
+                props={{
+                  chartVariants: [
+                      {
+                        type: 'Bar',
+                        props: {
+                          dataKey: 'customer',
+                          yLabels: ['value'],
+                        },
                       },
-                    },
-                ]
-              }}
-            />
-          )
-        }
-      </div>
-    </>
+                  ]
+                }}
+              />
+            )
+          }
+        </div>
+      </Paper>
   )}
   </>)
 }
