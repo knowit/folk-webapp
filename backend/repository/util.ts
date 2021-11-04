98a8a2732f0d3d378f861f4cb592510112741121
thomasduy <thomas_duy@hotmail.com>
2021-11-04 14:16:51 +0100

    fjernet funksjoner i utils som ikke brukes

--- backend/repository/util.ts
+++ backend/repository/util.ts
@@ -1,48 +1,7 @@
-import crypto from 'crypto'
 import AWS from 'aws-sdk'
 AWS.config.update({ region: 'eu-central-1' })
 
-const ssm = new AWS.SSM()
-
-export const getSecret = (name:string, { encrypted = false } = {}) => {
-  return new Promise((resolve, reject) => {
-    ssm.getParameter(
-      {
-        Name: name,
-        WithDecryption: encrypted,
-      },
-      (err, data) => {
-        if (err) reject(err)
-        else resolve(data && data.Parameter ? data.Parameter.Value : null)
-      }
-    )
-  })
-}
-
-export const makeEmailUuid = (email, salt) => {
-  const hmac = crypto.createHmac('sha256', salt)
-  hmac.update(email)
-  const sig = hmac.digest('hex')
-
-  return [
-    sig.substring(0, 8),
-    sig.substring(8, 12),
-    sig.substring(12, 16),
-    sig.substring(16, 20),
-    sig.substring(20, 32),
-  ].join('-')
-}
-
-export const groupBy = (items, key) =>
-  items.reduce(
-    (result, item) => ({
-      ...result,
-      [item[key]]: [...(result[item[key]] || []), item],
-    }),
-    {}
-  )
-
-export const range = (x, y) =>
+export const range = (x:number, y:number) =>
   Array.from(
     (function* () {
       while (x <= y) yield x++
@@ -92,48 +51,3 @@ export const mergeEmployees = (allEmployees:EmployeeInformation[]):MergeEmployee
 }
 
 
-export const reStructCategories = (categories, compScores = [], motScores = []) => {
-  //find the main categoreis
-  const mainCategories = new Set(
-    categories.flatMap((item) => Object.keys(item))
-  )
-  let catSet = []
-  const mainCats = []
-
-  // Merges the two arrays on the same category
-  const mergedArrs = compScores.map((i) => {
-    const found = motScores.find((j) => j.kategori === i.kategori)
-    const mergedObj = { ...found, ...i }
-    return mergedObj
-  })
-
-  mainCategories.forEach((name:string) => {
-    mainCats.push(mergedArrs.find((obj) => {
-      return obj['kategori'].toUpperCase() == name.toUpperCase()
-    }))
-
-    const categoryObject = {
-      [name]: [],
-    }
-    categories.forEach((item) => {
-      const childName = item[name]
-      if (childName) {
-        // Create child category
-        const foundSubCat = mergedArrs.find((obj) => {
-          return obj['kategori'].toUpperCase() === childName.toUpperCase()
-        })
-        categoryObject[name].push(foundSubCat)
-      }
-    })
-    catSet.push(categoryObject)
-  })
-  catSet.unshift({ Hovedkategorier: mainCats })
-  catSet = catSet.reduce(function (cat, x) {
-    for (const key in x) cat[key] = x[key]
-    return cat
-  }, {})
-
-  const setNames = Object.keys(catSet)
-
-  return [catSet, setNames]
-}
