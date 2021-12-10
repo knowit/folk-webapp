a9e6e66a0cea032300e60c212ac7ee73568342f8
Magnus L. Holtet <dev@holtet.me>
2021-12-10 12:42:18 +0100

    Remove dummy data

--- src/pages/customer/CustomerCardList.tsx
+++ src/pages/customer/CustomerCardList.tsx
@@ -4,33 +4,6 @@ import { DDPayload } from '../../data/types'
 import { useFetchedData } from '../../hooks/service'
 import CustomerCard, { CustomerData } from './CustomerCard'
 
-// const data = [
-//   {
-//     customer: 'customer 2',
-//     consultants: 1,
-//     billedLastPeriod: 6,
-//     billedTotal: 6,
-//   },
-//   {
-//     customer: 'customer 1',
-//     consultants: 1,
-//     billedLastPeriod: 4,
-//     billedTotal: 4,
-//   },
-//   {
-//     customer: 'Entur AS',
-//     consultants: 2,
-//     billedLastPeriod: 16,
-//     billedTotal: 32,
-//   },
-//   {
-//     customer: 'customer 3',
-//     consultants: 1,
-//     billedLastPeriod: 1,
-//     billedTotal: 1,
-//   },
-// ]
-
 const CustomerCardList = () => {
   const [data, pending, error] = useFetchedData<DDPayload>({
     url: '/api/data/customerCards',
