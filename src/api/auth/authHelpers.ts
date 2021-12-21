c2c10cad9e0532b66a6f25bea7dd10bcf8bbf339
Magnus L. Holtet <dev@holtet.me>
2021-12-21 19:21:08 +0100

    Create constants and fix return value

--- src/api/auth/authHelpers.ts
+++ src/api/auth/authHelpers.ts
@@ -1,36 +1,37 @@
-import { useUserInfo } from '../../context/UserInfoContext'
+import { UserInfo } from './authApiTypes'
+import { ACCESS_TOKEN, ACCESS_TOKEN_EXPIRES_AT } from './constants'
 
 // access_token
 export const setAccessToken = (token: string) =>
-  localStorage.setItem('access_token', token)
+  localStorage.setItem(ACCESS_TOKEN, token)
 
-export const getAccessToken = () => localStorage.getItem('access_token')
+export const getAccessToken = () => localStorage.getItem(ACCESS_TOKEN)
 
-export const removeAccessToken = () => localStorage.removeItem('access_token')
+export const removeAccessToken = () => localStorage.removeItem(ACCESS_TOKEN)
 
 // Expiration
 export const setAccessTokenExpiresAt = (expiresAt: number) =>
-  localStorage.setItem('access_token_expires_at', JSON.stringify(expiresAt))
+  localStorage.setItem(ACCESS_TOKEN_EXPIRES_AT, JSON.stringify(expiresAt))
 
 export const getAccessTokenExpiresAt = () => {
-  const expiresAt = localStorage.getItem('access_token_expires_at')
+  const expiresAt = localStorage.getItem(ACCESS_TOKEN_EXPIRES_AT)
   return expiresAt ? +expiresAt : 0
 }
 
 export const removeAccessTokenExpiresAt = () =>
-  localStorage.removeItem('access_token_expires_at')
+  localStorage.removeItem(ACCESS_TOKEN_EXPIRES_AT)
 
 // Consider using localStorage.clear()
 export const clearLocalStorage = () => {
   localStorage.clear()
 }
 
-export const isAccessTokenValid = () => {
+export const isAccessTokenValid = (expires_at: number) => {
   // Devided by 1000 to remove milliseconds
-  return new Date().getTime() / 1000 < getAccessTokenExpiresAt() - 10 // 10 second buffer
+  return new Date().getTime() / 1000 < expires_at - 10 // 10 second buffer
 }
 
-export const isLoggedIn = () => {
-  const user = useUserInfo()
-  return user && isAccessTokenValid()
+export const isLoggedIn = (user: UserInfo | null) => {
+  const expiresAt = getAccessTokenExpiresAt()
+  return user && isAccessTokenValid(expiresAt)
 }
