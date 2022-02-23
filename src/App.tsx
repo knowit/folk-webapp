ccb639b25eaba252903c166b83bf52d358de946a
unknown <larssoerlie96@gmail.com>
2022-02-23 18:07:26 +0100

    Fixed the build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
-import Header from './components/Header'
+import Header from './components/header/Header'
 import Content from './components/Content'
 import Footer from './components/Footer'
 
