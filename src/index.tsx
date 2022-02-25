f6cd4bfee119147f3631d2086d69f4a9afb588b7
Thomas B. Aschim <thomas.aschim@knowit.no>
2022-02-25 15:41:22 +0100

    Extracted theme to separate file

--- src/index.tsx
+++ src/index.tsx
@@ -2,46 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import { createTheme, ThemeProvider } from '@material-ui/core/styles'
+import { ThemeProvider } from '@material-ui/core/styles'
 import App from './App'
 import { UserInfoProvider } from './context/UserInfoContext'
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#707070',
-    },
-    background: {
-      default: '#FFFFFF',
-      paper: '#F1F0ED',
-    },
-    text: {
-      primary: '#333333',
-    },
-    error: {
-      main: '#802826',
-    },
-    success: {
-      main: '#1e561f',
-    },
-  },
-  spacing: 22.5,
-  typography: {
-    fontFamily: 'Arial,Helvetica Neue,Helvetica,sans-serif',
-    fontWeightRegular: 'normal',
-  },
-})
+import { theme } from './theme'
 
 ReactDOM.render(
-  <>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <BrowserRouter>
-        <UserInfoProvider>
-          <App />
-        </UserInfoProvider>
-      </BrowserRouter>
-    </ThemeProvider>
-  </>,
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <BrowserRouter>
+      <UserInfoProvider>
+        <App />
+      </UserInfoProvider>
+    </BrowserRouter>
+  </ThemeProvider>,
   document.getElementById('root')
 )
---
+++ src/theme.tsx
@@ -0,0 +1,31 @@
+import { createTheme } from '@material-ui/core/styles'
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#707070',
+      light: '#E4E1DB',
+    },
+    secondary: {
+      main: '#ff0000',
+    },
+    background: {
+      default: '#FFFFFF',
+      paper: '#F1F0ED',
+    },
+    text: {
+      primary: '#333333',
+    },
+    error: {
+      main: '#802826',
+    },
+    success: {
+      main: '#1e561f',
+    },
+  },
+  spacing: 22.5,
+  typography: {
+    fontFamily: 'Arial, Helvetica Neue, Helvetica, sans-serif',
+    fontWeightRegular: 'normal',
+  },
+})
