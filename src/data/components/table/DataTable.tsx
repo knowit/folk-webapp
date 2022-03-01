dfd81f6f5171c44dfcf8a77359f56671d2c80d79
Thomas B. Aschim <thomas.aschim@knowit.no>
2022-03-01 10:45:18 +0100

    A couple of minor fixes

--- src/data/components/table/DataTable.tsx
+++ src/data/components/table/DataTable.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useRef, useState } from 'react'
+import * as React from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 import { TableCell, withStyles } from '@material-ui/core'
 import Paper from '@material-ui/core/Paper'
--- src/data/types.tsx
+++ src/data/types.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import { SWRResponse } from 'swr'
 import { FilterObject } from '../components/FilterUtil'
 
