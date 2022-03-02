7639edab9abce3b154b3400acbefaf0260131b13
Magnus L. Holtet <dev@holtet.me>
2022-03-02 14:35:39 +0100

    Fix import

--- src/pages/Debug.tsx
+++ src/pages/Debug.tsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from 'react'
 import { getTestV2 } from '../api/client'
 import { useHoursBilledPerWeek } from '../api/data/customer/customerQueries'
 import { GridItem } from '../components/gridItem/GridItem'
-import { Grid } from '@material-ui/core'
 
 const Debug = () => {
   const [data, setData] = useState<any>()
