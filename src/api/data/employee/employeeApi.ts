5dcb81538304d784b5b3f8939e7a3ea8b9970666
Thomas B. Aschim <thomas.aschim@knowit.no>
2022-03-02 10:56:51 +0100

    Reverted API fetching functions

--- src/api/data/employee/employeeApi.ts
+++ src/api/data/employee/employeeApi.ts
@@ -9,18 +9,18 @@ import {
 export const getEmployeeTable = () =>
   getAtApi<EmployeeTableResponse[]>('/data/employeeTable')
 
-export const getEmployeeProfile = (endpoint: string, email: string) =>
-  getAtApi<EmployeeProfileResponse>(`/data${endpoint}`, {
+export const getEmployeeProfile = (url: string, email: string) =>
+  getAtApi<EmployeeProfileResponse>(`/data/employeeProfile`, {
     params: { email },
   })
 
-export const getEmployeeExperience = (endpoint: string, user_id: string) =>
-  getAtApi<EmployeeExperienceResponse>(`/data${endpoint}`, {
+export const getEmployeeExperience = (url: string, user_id: string) =>
+  getAtApi<EmployeeExperienceResponse>(`/data/employeeExperience`, {
     params: { user_id },
   })
 
 // ! This might not be typed correctly
-export const getEmployeeRadar = (endpoint: string, email: string) =>
-  getAtApi<CompetenceAreasResponse>(`/data${endpoint}`, {
+export const getEmployeeRadar = (url: string, email: string) =>
+  getAtApi<CompetenceAreasResponse>(`/data/employeeRadar`, {
     params: { email },
   })
