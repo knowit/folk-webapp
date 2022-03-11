167d8c46648d3dfdf4bd790828761584d8bbe2ee
Thomas B. Aschim <thomas.aschim@knowit.no>
2022-03-11 09:30:30 +0100

    Created type for new customer list endpoint

--- backend/routers/customer/customerTypes.ts
+++ backend/routers/customer/customerTypes.ts
@@ -1,3 +1,5 @@
+import { CvLinks } from '../employees/employeesTypes'
+
 export interface BilledCustomerHours {
   customer: string
   employees: number
@@ -16,4 +18,23 @@ export interface EmployeeWithPrimaryCustomer {
   image_key?: string
   customer_name: string
   work_order_description: string
+}
+
+export interface CustomerWithEmployees {
+  customer_name: string
+  employees: EmployeeForCustomerList[]
+}
+
+export type EmployeeForCustomerList = {
+  rowId: string
+  rowData: [
+    info: {
+      name: string
+      email: string
+      image_url: string | undefined
+    },
+    jobTitle: string | undefined,
+    customerAndProject: string,
+    cvLinks: CvLinks
+  ]
 }
