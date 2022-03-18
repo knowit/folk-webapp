713876a420b9fbe5e2b25695210884840dd6e846
Magnus L. Holtet <dev@holtet.me>
2022-03-18 13:10:43 +0100

    Update div height

--- src/components/charts/BarChart.tsx
+++ src/components/charts/BarChart.tsx
@@ -6,22 +6,20 @@ import React from 'react'
  * Nivo Bar Chart with custom styling.
  * Allows for overloading of all props for more detailed customization.
  */
-const BarChart: React.FC<BarSvgProps> = ({ ...props }) => {
-  return (
-    <div style={{ width: '100%', height: '50vh' }}>
-      <ResponsiveBar
-        margin={{ top: 40, right: 20, bottom: 65, left: 30 }}
-        enableLabel={false}
-        colors={chartColors}
-        borderRadius={3}
-        groupMode="grouped"
-        tooltip={({ indexValue, value }) => (
-          <div>{`${indexValue}: ${value}`}</div>
-        )}
-        {...props}
-      />
-    </div>
-  )
-}
+const BarChart: React.FC<BarSvgProps> = ({ ...props }) => (
+  <div style={{ width: '100%', height: '300px' }}>
+    <ResponsiveBar
+      margin={{ top: 40, right: 20, bottom: 65, left: 30 }}
+      enableLabel={false}
+      colors={chartColors}
+      borderRadius={3}
+      groupMode="grouped"
+      tooltip={({ indexValue, value }) => (
+        <div>{`${indexValue}: ${value}`}</div>
+      )}
+      {...props}
+    />
+  </div>
+)
 
 export default BarChart
--- src/components/charts/PieChart.tsx
+++ src/components/charts/PieChart.tsx
@@ -3,24 +3,22 @@ import React from 'react'
 import { PieChartData } from '../../api/data/chartResponses'
 import { chartColors } from './common'
 
-const PieChart = ({ id, value, data }: PieChartData) => {
-  return (
-    <div style={{ width: '100%', height: '50vh' }}>
-      <ResponsivePie
-        id={id}
-        value={value}
-        data={data}
-        margin={{ top: 40, right: 20, bottom: 65, left: 30 }}
-        innerRadius={0.5}
-        padAngle={5}
-        cornerRadius={10}
-        colors={chartColors}
-        borderWidth={1}
-        sliceLabelsSkipAngle={10}
-        radialLabel={(e) => `${e.label} (${e.value})`}
-      />
-    </div>
-  )
-}
+const PieChart = ({ id, value, data }: PieChartData) => (
+  <div style={{ width: '100%', height: '300px' }}>
+    <ResponsivePie
+      id={id}
+      value={value}
+      data={data}
+      margin={{ top: 40, right: 20, bottom: 65, left: 30 }}
+      innerRadius={0.5}
+      padAngle={5}
+      cornerRadius={10}
+      colors={chartColors}
+      borderWidth={1}
+      sliceLabelsSkipAngle={10}
+      radialLabel={(e) => `${e.label} (${e.value})`}
+    />
+  </div>
+)
 
 export default PieChart
