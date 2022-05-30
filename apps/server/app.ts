b7c760ece8fdba3aea943f57335bc0238e93ed03
Magnus L. Holtet <dev@holtet.me>
2022-05-30 14:28:36 +0200

    trigger

--- apps/server/app.ts
+++ apps/server/app.ts
@@ -5,7 +5,6 @@ import { errorHandler, NotFoundError } from './middlewares/errorHandling'
 import { apiRouterV2 } from './routers/routers'
 
 const app = express()
-console.log("trigger deploy")
 
 // Register Middleware
 app.use(cookieParser())
