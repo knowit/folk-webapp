57e3907c715fbcccb81a77227dc08ed60ac862f3
Henrik Aspenes Vedal <henrikvedal@Henriks-MacBook-Pro-73760.local>
2022-06-15 10:26:48 +0200

    Fikset kommentarer på PR og skjult filter slik at det ikke påvirker prod før filtrene er implementert riktig

--- apps/web/src/pages/customer/cards/HoursBilledPerCustomerCard.tsx
+++ apps/web/src/pages/customer/cards/HoursBilledPerCustomerCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ChartCard from '../../../components/charts/ChartCard'
 import { useHoursBilledPerCustomerCharts } from '../../../api/data/customer/customerQueries'
+import { POSSIBLE_OLD_DATA_WARNING } from './messages'
 
 const HoursBilledPerCustomerCard = () => {
   const { data, error } = useHoursBilledPerCustomerCharts()
@@ -8,10 +9,10 @@ const HoursBilledPerCustomerCard = () => {
   return (
     <ChartCard
       title="Timer brukt per kunde"
-      description="Dataene er fra første registrering i UBW. Noen av dataene kan være justert i etterkant, og disse justeringene er ikke oppdatert her."
+      description={POSSIBLE_OLD_DATA_WARNING}
       data={data}
       error={error}
-      showFilter={true}
+      showFilter={false}
     />
   )
 }
---
+++ apps/web/src/pages/customer/cards/messages.ts
@@ -0,0 +1,2 @@
+export const POSSIBLE_OLD_DATA_WARNING =
+  'Dataene er fra første registrering i UBW. Noen av dataene kan være justert i etterkant, og disse justeringene er ikke oppdatert her.'
