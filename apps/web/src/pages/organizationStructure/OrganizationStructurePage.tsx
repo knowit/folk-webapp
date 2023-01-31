1263a870b93f4af8ea831d3148b158fbdf8c9273
matfol <mats.follestad@knowit.no>
2023-01-31 12:14:58 +0100

    removed unused error

--- apps/web/src/pages/organizationStructure/OrganizationStructurePage.tsx
+++ apps/web/src/pages/organizationStructure/OrganizationStructurePage.tsx
@@ -3,7 +3,7 @@ import { useEmployeeStructure } from '../../api/data/employee/employeeQueries'
 import OrganizationStructureTree from './Components/OrganizationStructureTree'
 
 export default function OrganizationStructurePage() {
-  const { data: employeeData, error } = useEmployeeStructure()
+  const { data: employeeData } = useEmployeeStructure()
   return (
     <div>
       <OrganizationStructureTree employeeStructureData={employeeData} />
