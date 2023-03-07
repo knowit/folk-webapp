fa78b1b54382782848c83bda74f6d791f8cc3166
Lars Gustavsen <larsgus90@gmail.com>
2023-03-07 16:19:47 +0100

    Improve skeleton layout for CustomerCardList

--- apps/web/src/pages/customer/components/CustomerCardList.tsx
+++ apps/web/src/pages/customer/components/CustomerCardList.tsx
@@ -2,12 +2,21 @@ import { Skeleton } from '@material-ui/lab'
 import React from 'react'
 import { useCustomerCards } from '../../../api/data/customer/customerQueries'
 import CustomerCard from '../cards/CustomerCard'
+import { GridItem } from 'web/src/components/gridItem/GridItem'
 
 const CustomerCardList = () => {
   const { data } = useCustomerCards()
 
   if (!data) {
-    return <Skeleton variant="rect" height={780} animation="wave" />
+    return (
+      <>
+        {[...Array(4)].map((_, index) => (
+          <GridItem key={index}>
+            <Skeleton variant="rect" height={175} />
+          </GridItem>
+        ))}
+      </>
+    )
   }
   return (
     <>
