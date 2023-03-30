b80cbcccf47dcc0dde09f4769062441a0f5e9c84
Lars Gustavsen <larsgus90@gmail.com>
2023-03-30 23:29:21 +0200

    Replace Dropdown in HoursBilledPerWeekCard with radio buttons

--- apps/web/src/pages/customer/cards/HoursBilledPerWeekCard.tsx
+++ apps/web/src/pages/customer/cards/HoursBilledPerWeekCard.tsx
@@ -4,7 +4,15 @@ import { useHoursBilledPerWeekCharts } from '../../../api/data/customer/customer
 import { POSSIBLE_OLD_DATA_WARNING } from './messages'
 import HoursBilledPerWeekTooltip from '../components/HoursBilledPerWeekTooltip'
 import useFilteredData from '../../../components/charts/chartFilters/useFilteredData'
-import DropdownPicker from '../../../components/charts/DropdownPicker'
+import { GridItem } from '../../../components/gridItem/GridItem'
+import {
+  Box,
+  RadioGroup,
+  FormLabel,
+  FormControlLabel,
+  Radio,
+  FormControl,
+} from '@material-ui/core'
 
 const HoursBilledPerWeekCard = () => {
   const { data, error } = useHoursBilledPerWeekCharts()
@@ -15,20 +23,44 @@ const HoursBilledPerWeekCard = () => {
   const chartData = getFilteredData()
 
   return (
-    <ChartCard
-      title="Timer brukt per periode"
-      description={POSSIBLE_OLD_DATA_WARNING}
-      data={chartData}
-      error={error}
-      sliceTooltip={HoursBilledPerWeekTooltip}
-      extraHeaderContent={
-        <DropdownPicker
-          values={filterOptions}
-          selected={selectedFilter}
-          onChange={setSelectedFilter}
-        />
-      }
-    />
+    <GridItem fullSize={true}>
+      <ChartCard
+        title="Timer brukt per periode"
+        description={POSSIBLE_OLD_DATA_WARNING}
+        data={chartData}
+        error={error}
+        fullSize={true}
+        sliceTooltip={HoursBilledPerWeekTooltip}
+        extraHeaderContent={
+          <FormControl component="fieldset">
+            <FormLabel>Grupper etter</FormLabel>
+            <Box display="flex" flexWrap="nowrap" width="100%">
+              <RadioGroup
+                style={{ flexWrap: 'nowrap' }}
+                row
+                value={selectedFilter}
+                onChange={(event) => {
+                  const option = filterOptions.find(
+                    (option) => option === event.target.value
+                  )
+                  setSelectedFilter(option)
+                }}
+              >
+                {filterOptions.map((option) => (
+                  <FormControlLabel
+                    key={option}
+                    value={option}
+                    control={<Radio />}
+                    label={option}
+                    style={{ flexGrow: 1 }}
+                  />
+                ))}
+              </RadioGroup>
+            </Box>
+          </FormControl>
+        }
+      />
+    </GridItem>
   )
 }
 
