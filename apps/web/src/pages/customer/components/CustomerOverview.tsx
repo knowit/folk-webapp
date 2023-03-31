1ac21ac31a98aa3b6789f3afca0d2ddd210c9692
Lars Gustavsen <larsgus90@gmail.com>
2023-03-31 12:31:42 +0200

    Remove unused import

--- apps/web/src/pages/customer/components/CustomerOverview.tsx
+++ apps/web/src/pages/customer/components/CustomerOverview.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import CustomerCardList from './CustomerCardList'
 import { Grid } from '@material-ui/core'
-import { HoursBilledPerCustomerCard, HoursBilledPerWeekCard } from '../cards'
+import { HoursBilledPerWeekCard } from '../cards'
 
 export const CustomerOverview = () => {
   return (
