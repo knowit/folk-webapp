289f43ee8b0f3306cc4b2ea4af390d5667fc0919
Gaute Rauboti <gaute.rauboti@knowit.no>
2023-04-18 15:35:08 +0200

    Fixed UI bugs

--- apps/web/src/theme.tsx
+++ apps/web/src/theme.tsx
@@ -1,33 +1,31 @@
-import { createTheme, adaptV4Theme } from '@mui/material/styles'
+import { createTheme } from '@mui/material/styles'
 
-export const theme = createTheme(
-  adaptV4Theme({
-    palette: {
-      primary: {
-        main: '#707070',
-        light: '#E4E1DB',
-      },
-      secondary: {
-        main: '#ff0000',
-      },
-      background: {
-        default: '#FFFFFF',
-        paper: '#F1F0ED',
-      },
-      text: {
-        primary: '#333333',
-      },
-      error: {
-        main: '#802826',
-      },
-      success: {
-        main: '#1e561f',
-      },
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#707070',
+      light: '#E4E1DB',
     },
-    spacing: 22.5,
-    typography: {
-      fontFamily: 'Arial, Helvetica Neue, Helvetica, sans-serif',
-      fontWeightRegular: 'normal',
+    secondary: {
+      main: '#ff0000',
     },
-  })
-)
+    background: {
+      default: '#FFFFFF',
+      paper: '#F1F0ED',
+    },
+    text: {
+      primary: '#333333',
+    },
+    error: {
+      main: '#802826',
+    },
+    success: {
+      main: '#1e561f',
+    },
+  },
+  spacing: 22.5,
+  typography: {
+    fontFamily: 'Arial, Helvetica Neue, Helvetica, sans-serif',
+    fontWeightRegular: 'normal',
+  },
+})
