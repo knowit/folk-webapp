d3e17364aac94087fa9fc2aa0b9531017af23744
Gaute Rauboti <gaute@rauboti.no>
2023-04-28 14:49:14 +0200

    Updated the EmployeeNotFound component

--- apps/web/src/pages/employee/components/EmployeeNotFound.tsx
+++ apps/web/src/pages/employee/components/EmployeeNotFound.tsx
@@ -1,26 +1,22 @@
 import * as React from 'react'
-import { makeStyles } from '@mui/styles'
+import { styled } from '@mui/material/styles'
 
-const useStyles = makeStyles({
-  root: {
-    padding: '0 25px',
-  },
-  header: {
-    marginBottom: '0',
-  },
-})
+const ComponentRoot = styled('article')(() => ({
+  adding: '0 25px',
+}))
+const ComponentHeader = styled('h1')(() => ({
+  marginBottom: 0,
+}))
 
 interface Props {
   employeeId: string
 }
 
 export function EmployeeNotFound({ employeeId }: Props) {
-  const classes = useStyles()
-
   return (
-    <article className={classes.root}>
-      <h1 className={classes.header}>Fant ingen ansatte med oppgitt ID</h1>
+    <ComponentRoot>
+      <ComponentHeader>Fant ingen ansatte med oppgitt ID</ComponentHeader>
       <p>{`Vennligst kontrollér ID-en (${employeeId}) som er oppgitt i adressefeltet.`}</p>
-    </article>
+    </ComponentRoot>
   )
 }
