5719c4e3c051f449ed129d6c2b085cc9fd479807
Lars Gustavsen <larsgus90@gmail.com>
2023-05-07 23:58:37 +0200

    Add pageview tracking using Matomo

--- apps/web/src/components/Content.tsx
+++ apps/web/src/components/Content.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { isLoggedIn } from '../api/auth/authHelpers'
 import { useUserInfo } from '../context/UserInfoContext'
@@ -13,9 +13,17 @@ import {
   OrganizationStructurePage,
 } from '../pages'
 import LoginPage from '../pages/login/LoginPage'
+import { useMatomo } from '@jonkoops/matomo-tracker-react'
+import { useLocation } from 'react-router-dom'
 
 export default function Content() {
   const { user } = useUserInfo()
+  const { trackPageView } = useMatomo()
+  const location = useLocation()
+
+  useEffect(() => {
+    trackPageView()
+  }, [trackPageView, location.pathname])
 
   if (!isLoggedIn(user)) {
     return <LoginPage />
