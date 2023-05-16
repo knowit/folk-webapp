38cfa1c72f8ed752f6a92fccc6a6fd65724eb902
Marie Fremstad <marie.fremstad@knowit.no>
2023-05-16 12:15:56 +0200

    Revert "Merge branch 'testing_organisasjonskartet' into #434-organisasjonsstruktur"
    This reverts commit d75f0666a2d73f4e0cc74db74a1b29e21ef23fd1.

--- apps/web/src/pages/organizationStructure/OrganizationStructurePage.tsx
+++ apps/web/src/pages/organizationStructure/OrganizationStructurePage.tsx
@@ -3,26 +3,9 @@ import OrganizationStructureTree from './Components/OrganizationStructureTree'
 import { BaseSkeleton } from '../../components/skeletons/BaseSkeleton'
 import { FallbackMessage } from '../employee/components/FallbackMessage'
 import { pageTitle } from '../../utils/pagetitle'
-import { useState } from 'react'
-import { Checkbox, CheckboxProps, FormControlLabel } from '@mui/material'
-import { withStyles } from '@mui/styles'
-import OSTtest from './Components/OSTtest'
-import data2 from './data.json'
-
-const BlackCheckBox = withStyles({
-  root: {
-    color: '#333333',
-    '&:hover': {
-      backgroundColor: 'transparent',
-    },
-  },
-})((props: CheckboxProps) => (
-  <Checkbox color="default" disableRipple {...props} />
-))
 
 export default function OrganizationStructurePage() {
   const { data, isLoading, error } = useEmployeeStructure()
-  const [collapsChildren, setCollapsChildren] = useState(false)
 
   pageTitle('Organisasjonsstruktur')
 
@@ -36,35 +19,12 @@ export default function OrganizationStructurePage() {
     return <FallbackMessage error={error} />
   }
 
-  function toggleDisplay() {
-    setCollapsChildren(!collapsChildren)
-  }
-
-  const checkBox = {
-    label: 'Skjul barn',
-    changeHandler: toggleDisplay,
-    checked: collapsChildren,
-  }
-
   return (
-    <>
-      <div style={{ width: '60%' }}>
-        <FormControlLabel
-          control={<BlackCheckBox onChange={checkBox.changeHandler} />}
-          label={checkBox.label}
-          checked={checkBox.checked}
-        />{' '}
-      </div>
-
-      {/**    <OrganizationStructureTree
-        collapsChildren={collapsChildren}
-        data={data}
-        width={1215}
-        height={1200}
-        margin={140}
-      />
-      */}
-      <OSTtest data={data2} />
-    </>
+    <OrganizationStructureTree
+      data={data}
+      width={1215}
+      height={1200}
+      margin={140}
+    />
   )
 }
