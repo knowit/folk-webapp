0f47bc796dcb936a132a9489e291a2ae9290dae8
KristianFlock <kristian.flock@knowit.no>
2023-08-10 14:01:13 +0200

    Adds functionality to get files from databricks

---
+++ apps/server/dataplattform/databricksS3Call.ts
@@ -0,0 +1,38 @@
+import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { fromIni } from '@aws-sdk/credential-providers'
+
+const client = new S3Client({
+  region: 'eu-west-1',
+  credentials: fromIni({ profile: '216105769281_AdministratorAccess' }),
+})
+
+export async function getFileFromS3(report: string): Promise<string> {
+  let res: string
+  // const key = getFilePath(report)
+  const command = new GetObjectCommand({
+    Bucket: 'knowit-databricks-u06vfj-external',
+    Key: 'some/reports/' + report + '.json',
+  })
+
+  try {
+    const response = await client.send(command)
+    // The Body object also has 'transformToByteArray' and 'transformToWebStream' methods.
+    const str = await response.Body?.transformToString()
+    if (typeof str == 'string') {
+      res = str
+    } else {
+      throw new Error('Error: Call returned empty')
+    }
+  } catch (err) {
+    console.log(err)
+  }
+  return Promise.resolve(ndjsonToJson(res))
+}
+
+function ndjsonToJson(data: string): string {
+  if (data == undefined) {
+    return '[]'
+  } else {
+    return '[' + data.split('\n').join(',').slice(0, -1) + ']'
+  }
+}
--- apps/server/routers/competence/competenceRouter.ts
+++ apps/server/routers/competence/competenceRouter.ts
@@ -23,16 +23,23 @@ import {
   YearsWorkingDistributionCount,
 } from './competenceTypes'
 
+import { getFileFromS3 } from '../../dataplattform/databricksS3Call'
+
 const router: Router = express.Router()
 
 router.get('/experienceDistribution', async (req, res, next) => {
   try {
-    const data = await getReport<YearsWorkingDistributionCount[]>({
-      accessToken: req.accessToken,
-      reportName: 'workExperienceDistributedInYears',
+    // const data = await getReport<YearsWorkingDistributionCount[]>({
+    //   accessToken: req.accessToken,
+    //   reportName: 'workExperienceDistributedInYears',
+    // })
+    // const aggregatedData = experienceDistribution(data)
+    // res.send(aggregatedData)
+    getFileFromS3('workExperienceDistributedInYears').then((result) => {
+      const data = JSON.parse(result)
+      const aggregatedData = experienceDistribution(data)
+      res.send(aggregatedData)
     })
-    const aggregatedData = experienceDistribution(data)
-    res.send(aggregatedData)
   } catch (error) {
     next(error)
   }
@@ -40,12 +47,18 @@ router.get('/experienceDistribution', async (req, res, next) => {
 
 router.get('/competenceAmount', async (req, res, next) => {
   try {
-    const data = await getReport<CompetenceAmount[]>({
-      accessToken: req.accessToken,
-      reportName: 'competenceAmountAggregated',
+    // const data = await getReport<CompetenceAmount[]>({
+    //   accessToken: req.accessToken,
+    //   reportName: 'competenceAmountAggregated',
+    // })
+    // console.log(data)
+    // const aggregatedData = competenceAmount(data)
+    // res.send(aggregatedData)
+    getFileFromS3('competenceAmountAggregated').then((result) => {
+      const data = JSON.parse(result)
+      const aggregatedData = competenceAmount(data)
+      res.send(aggregatedData)
     })
-    const aggregatedData = competenceAmount(data)
-    res.send(aggregatedData)
   } catch (error) {
     next(error)
   }
@@ -53,12 +66,17 @@ router.get('/competenceAmount', async (req, res, next) => {
 
 router.get('/competenceAreas', async (req, res, next) => {
   try {
-    const data = await getReport<CategoryAverage[]>({
-      accessToken: req.accessToken,
-      reportName: 'averageCompetenceAndMotivation',
+    // const data = await getReport<CategoryAverage[]>({
+    //   accessToken: req.accessToken,
+    //   reportName: 'averageCompetenceAndMotivation',
+    // })
+    // const aggregatedData = competenceAreas(data)
+    // res.send(aggregatedData)
+    getFileFromS3('averageCompetenceAndMotivation').then((result) => {
+      const data = JSON.parse(result)
+      const aggregatedData = competenceAreas(data)
+      res.send(aggregatedData)
     })
-    const aggregatedData = competenceAreas(data)
-    res.send(aggregatedData)
   } catch (error) {
     next(error)
   }
@@ -66,27 +84,35 @@ router.get('/competenceAreas', async (req, res, next) => {
 
 router.get('/ageDistribution', async (req, res, next) => {
   try {
-    const ageDistributionPromise = getReport<AgeDistribution[]>({
-      accessToken: req.accessToken,
-      reportName: 'ageDistribution',
+    // const ageDistributionPromise = getReport<AgeDistribution[]>({
+    //   accessToken: req.accessToken,
+    //   reportName: 'ageDistribution',
+    // })
+
+    // const ageDistributionGroupsPromise = getReport<AgeGroupDistribution[]>({
+    //   accessToken: req.accessToken,
+    //   reportName: 'ageDistributionGroups',
+    // })
+
+    // const [ageDistributionData, ageDistributionGroups] = await Promise.all([
+    //   ageDistributionPromise,
+    //   ageDistributionGroupsPromise,
+    // ])
+
+    // const aggregatedData = ageDistribution([
+    //   ageDistributionData,
+    //   ageDistributionGroups,
+    // ])
+
+    // res.send(aggregatedData)
+    getFileFromS3('ageDistribution').then((age) => {
+      getFileFromS3('ageDistributionGroups').then((groups) => {
+        const age_data = JSON.parse(age)
+        const groups_data = JSON.parse(groups)
+        const aggregatedData = ageDistribution([age_data, groups_data])
+        res.send(aggregatedData)
+      })
     })
-
-    const ageDistributionGroupsPromise = getReport<AgeGroupDistribution[]>({
-      accessToken: req.accessToken,
-      reportName: 'ageDistributionGroups',
-    })
-
-    const [ageDistributionData, ageDistributionGroups] = await Promise.all([
-      ageDistributionPromise,
-      ageDistributionGroupsPromise,
-    ])
-
-    const aggregatedData = ageDistribution([
-      ageDistributionData,
-      ageDistributionGroups,
-    ])
-
-    res.send(aggregatedData)
   } catch (error) {
     next(error)
   }
@@ -94,13 +120,17 @@ router.get('/ageDistribution', async (req, res, next) => {
 
 router.get('/fagtimer', async (req, res, next) => {
   try {
-    const data = await getReport<FagtimeStats[]>({
-      accessToken: req.accessToken,
-      reportName: 'fagActivity',
+    // const data = await getReport<FagtimeStats[]>({
+    //   accessToken: req.accessToken,
+    //   reportName: 'fagActivity',
+    // })
+    // const aggregatedData = fagtimer(data)
+    // res.send(aggregatedData)
+    getFileFromS3('fagActivity').then((result) => {
+      const data = JSON.parse(result)
+      const aggregatedData = fagtimer(data)
+      res.send(aggregatedData)
     })
-
-    const aggregatedData = fagtimer(data)
-    res.send(aggregatedData)
   } catch (error) {
     next(error)
   }
@@ -115,6 +145,11 @@ router.get('/fagEvents', async (req, res, next) => {
 
     const aggregatedData = fagEventsLine(data)
     res.send(aggregatedData)
+    // getFileFromS3('fagEvents').then((result) => {
+    //   const data = JSON.parse(result)
+    //   const aggregatedData = fagEventsLine(data)
+    //   res.send(aggregatedData)
+    // })
   } catch (error) {
     next(error)
   }
@@ -122,26 +157,34 @@ router.get('/fagEvents', async (req, res, next) => {
 
 router.get('/education', async (req, res, next) => {
   try {
-    const degreeDistributionPromise = getReport<DegreeDistribution[]>({
-      accessToken: req.accessToken,
-      reportName: 'degreeDist',
+    // const degreeDistributionPromise = getReport<DegreeDistribution[]>({
+    //   accessToken: req.accessToken,
+    //   reportName: 'degreeDist',
+    // })
+
+    // const numberOfEmployeesPromise = getReport<NumberOfEmployees[]>({
+    //   accessToken: req.accessToken,
+    //   reportName: 'countEmployees',
+    // })
+
+    // const [degreeDistributionData, numberOfEmployeesData] = await Promise.all([
+    //   degreeDistributionPromise,
+    //   numberOfEmployeesPromise,
+    // ])
+
+    // const aggregatedData = educationPie(
+    //   degreeDistributionData,
+    //   numberOfEmployeesData
+    // )
+    // res.send(aggregatedData)
+    getFileFromS3('degreeDist').then((degree) => {
+      getFileFromS3('countEmployees').then((count) => {
+        const degree_data = JSON.parse(degree)
+        const count_data = JSON.parse(count)
+        const aggregatedData = educationPie(degree_data, count_data)
+        res.send(aggregatedData)
+      })
     })
-
-    const numberOfEmployeesPromise = getReport<NumberOfEmployees[]>({
-      accessToken: req.accessToken,
-      reportName: 'countEmployees',
-    })
-
-    const [degreeDistributionData, numberOfEmployeesData] = await Promise.all([
-      degreeDistributionPromise,
-      numberOfEmployeesPromise,
-    ])
-
-    const aggregatedData = educationPie(
-      degreeDistributionData,
-      numberOfEmployeesData
-    )
-    res.send(aggregatedData)
   } catch (error) {
     next(error)
   }
@@ -149,13 +192,18 @@ router.get('/education', async (req, res, next) => {
 
 router.get('/competenceMapping', async (req, res, next) => {
   try {
-    const data = await getReport<CategoryAverage[]>({
-      accessToken: req.accessToken,
-      reportName: 'newCompetenceMotivationAverages',
-    })
+    // const data = await getReport<CategoryAverage[]>({
+    //   accessToken: req.accessToken,
+    //   reportName: 'newCompetenceMotivationAverages',
+    // })
 
-    const aggregatedData = competenceMappingConversion(data)
-    res.send(aggregatedData)
+    // const aggregatedData = competenceMappingConversion(data)
+    // res.send(aggregatedData)
+    getFileFromS3('newCompetenceMotivationAverages').then((result) => {
+      const data = JSON.parse(result)
+      const aggregatedData = competenceMappingConversion(data)
+      res.send(aggregatedData)
+    })
   } catch (error) {
     next(error)
   }
@@ -163,17 +211,21 @@ router.get('/competenceMapping', async (req, res, next) => {
 
 router.get('/competenceFilter', async (req, res, next) => {
   try {
-    const data = await getReport<CompetenceFilterRawData[]>({
-      accessToken: req.accessToken,
-      reportName: 'newCategories',
+    // const data = await getReport<CompetenceFilterRawData[]>({
+    //   accessToken: req.accessToken,
+    //   reportName: 'newCategories',
+    // })
+
+    // const aggregatedData = data.map((e) => ({
+    //   category: e.category,
+    //   subCategories: JSON.parse(e.subCategories),
+    // }))
+
+    // res.send(aggregatedData)
+    getFileFromS3('newCategories').then((result) => {
+      const data = JSON.parse(result)
+      res.send(data)
     })
-
-    const aggregatedData = data.map((e) => ({
-      category: e.category,
-      subCategories: JSON.parse(e.subCategories),
-    }))
-
-    res.send(aggregatedData)
   } catch (error) {
     next(error)
   }
