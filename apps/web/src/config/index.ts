95f5822735071475b6c9d2158e3df6d47ef0dede
Joacim Delfin Sveen <joacim.sveen@knowit.no>
2024-06-07 14:04:40 +0200

    Forgot console.log change in last. Reverted change on .env.

--- apps/web/src/config/index.ts
+++ apps/web/src/config/index.ts
@@ -4,12 +4,13 @@ import { createProdConfig } from './envs/prod'
 export const appConfig = getConfig()
 
 function getConfig() {
-  switch (process.env.APP_ENV) {
+  console.log('Environment:', process.env.REACT_APP_ENV)
+  switch (process.env.REACT_APP_ENV) {
     case 'production':
       return createProdConfig()
     case 'development':
       return createDevConfig()
     default:
-      throw new Error('Invalid APP_ENV "${process.env.APP_ENV}"')
+      throw new Error(`Invalid REACT_APP_ENV ${process.env.REACT_APP_ENV}`)
   }
 }
