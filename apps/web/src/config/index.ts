ade3a88913858ca6de73de3826a88724304b5f76
Joacim Delfin Sveen <joacim.sveen@knowit.no>
2024-06-10 12:41:41 +0200

    Removed console.log() that are no longer needed

--- apps/web/src/config/index.ts
+++ apps/web/src/config/index.ts
@@ -4,8 +4,6 @@ import { createProdConfig } from './envs/prod'
 export const appConfig = getConfig()
 
 function getConfig() {
-  console.log('NODE_ENV:', process.env.NODE_ENV)
-  console.log('Environment:', process.env.REACT_APP_ENV)
   switch (process.env.REACT_APP_ENV) {
     case 'production':
       return createProdConfig()
