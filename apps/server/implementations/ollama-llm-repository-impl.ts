b4a28ab81496d9065e9bd39015e11777a34b322f
palmanshaus <pal.manshaus@knowit.no>
2024-12-09 09:41:01 +0100

    Very wip

--- apps/server/implementations/ollama-llm-repository-impl.ts
+++ apps/server/implementations/ollama-llm-repository-impl.ts
@@ -7,15 +7,65 @@ import {
   Tool,
 } from '../repository/llm-repository'
 
-class OllamaLLMRepositoryImpl extends LLMClient {
-  generateReply(
+export class OllamaLLMRepositoryImpl extends LLMClient {
+  constructor() {
+    super()
+    this.client = 'http://localhost:11434/api/chat'
+  }
+
+  client: string
+
+  async generateReply(
     model: string,
     messages: LLMMessage[],
     tools?: Tool[],
     options?: Record<string, any>
   ): Promise<LLMResponse> {
-    throw new Error('Method not implemented.')
+    const body = {
+      model: 'llama3.1',
+      messages: [{ role: 'user', content: prompt }],
+    }
+
+    try {
+      const response = await fetch(this.client, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+      })
+      if (!response.ok) {
+        console.error('Error generating reply:', response.text)
+        return
+      }
+
+      const reader = response.body?.getReader()
+      const textDecoder = new TextDecoder()
+
+      if (!reader) {
+        console.error('No response body')
+        return
+      }
+
+      // Read and stream Ollama's responses
+      // eslint-disable-next-line no-constant-condition
+      while (true) {
+        const { value, done } = await reader.read()
+        if (done) break
+
+        let content = textDecoder.decode(value)
+        const transformedContent = JSON.parse(content)
+        content = JSON.stringify(transformedContent.message['content'])
+        content = content.slice(1, -1)
+        if (content) {
+          observer.next(content)
+        }
+      }
+
+      observer.complete()
+    } catch (error) {
+      observer.error(error)
+    }
   }
+
   generateStream(
     model: string,
     messages: LLMMessage[],
