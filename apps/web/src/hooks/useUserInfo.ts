581b1875d0015698858664fe0f5ff350d96f065e
palmanshaus <pal.manshaus@knowit.no>
2024-12-10 10:17:46 +0100

    Remove useuserinfo-hack

--- apps/web/src/hooks/useUserInfo.ts
+++ apps/web/src/hooks/useUserInfo.ts
@@ -3,7 +3,7 @@ import { UserInfoContext } from '../context/UserInfoContext'
 import { useAuthenticator } from '@aws-amplify/ui-react'
 
 export function useUserInfo() {
-  const [isAuthenticated, setIsAuthenticated] = useState(true)
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
   const userInfoContext = useContext(UserInfoContext)
   const { authStatus } = useAuthenticator((context) => [context.authStatus])
   const { user, setUser, userEmployeeProfile } = userInfoContext
