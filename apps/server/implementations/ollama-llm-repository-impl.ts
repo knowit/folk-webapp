ff98153bf2abf14bd5ab60ccd9cb75a0dea250f3
Brynje Nordli <brynje@gmail.com>
2024-12-11 12:56:03 +0100

    Feature/ollama impl (#631)
    * Preliminary implementation, missing some tests
    
    * Somewhat working ollama implementation
    
    * Removed Ollama from packages as it is not used
    
    * Fixed embedding and tests

--- apps/server/implementations/ollama-llm-repository-impl.ts
+++ apps/server/implementations/ollama-llm-repository-impl.ts
@@ -4,30 +4,304 @@ import {
   LLMClient,
   LLMMessage,
   LLMResponse,
+  LLMRole,
   Tool,
 } from '../repository/llm-repository'
+import {
+  OllamaEmbedding,
+  OllamaV1Chunk,
+  OllamaV1Response,
+} from './dto/ollama-v1-responses'
 
-class OllamaLLMRepositoryImpl extends LLMClient {
-  generateReply(
+export class OllamaLLMRepositoryImpl extends LLMClient {
+  private baseUrl: string
+
+  constructor(baseUrl: string) {
+    super()
+    this.baseUrl = baseUrl
+  }
+
+  private async postReply(
+    endpoint: string,
+    body: Record<string, any>
+  ): Promise<OllamaV1Response> {
+    const response = await fetch(`${this.baseUrl}${endpoint}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+    console.log('Body: ' + JSON.stringify(body))
+    const rawText = await response.text()
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status} - ${rawText}`)
+    }
+    return OllamaV1Response.fromJson(JSON.parse(rawText))
+  }
+
+  async generateReply(
     model: string,
     messages: LLMMessage[],
     tools?: Tool[],
     options?: Record<string, any>
   ): Promise<LLMResponse> {
-    throw new Error('Method not implemented.')
+    const body = {
+      model,
+      messages: toOllamaMessages(messages),
+      tools: toOllamaTools(tools),
+      stream: false,
+      options,
+    }
+    const chatResponse: OllamaV1Response = await this.postReply(
+      '/v1/chat/completions',
+      body
+    )
+    console.log(
+      'Messages:' + JSON.stringify(toOllamaMessages(messages), null, 2)
+    )
+    const toolCalls = chatResponse.choices[0].message.toolCalls
+    if (toolCalls != null && toolCalls.length > 0) {
+      const toolCall = toolCalls[0]
+      messages.push({
+        content: null,
+        role: LLMRole.assistant,
+        toolCalls: [toolCall],
+      })
+      const tool =
+        tools.find((tool) => tool.name == toolCall.function.name) || null
+      const toolArguments = toolCall.function.arguments
+      const toolResponse =
+        (await tool?.use(toolArguments ? JSON.parse(toolArguments) : null)) ??
+        'Unable to use non-existing tool ' + toolCall.function.name
+      messages.push({
+        role: LLMRole.tool,
+        toolCallId: toolCall.id,
+        content: toolResponse,
+      })
+      return this.generateReply(model, messages, tools, options)
+    }
+    return { content: chatResponse.choices[0].message.content }
   }
+
   generateStream(
     model: string,
     messages: LLMMessage[],
     tools?: Tool[],
     options?: Record<string, any>
   ): AsyncGenerator<LLMChunk> {
-    throw new Error('Method not implemented.')
+    const body = {
+      model,
+      messages: toOllamaMessages(messages),
+      tools: toOllamaTools(tools),
+      stream: true,
+      options,
+    }
+    console.log(
+      'Messages:' + JSON.stringify(toOllamaMessages(messages), null, 2)
+    )
+    return async function* () {
+      const response = await fetch(`${this.baseUrl}/v1/chat/completions`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify(body),
+      })
+
+      if (!response.ok) {
+        const rawBody = await response.text()
+        throw new Error(`HTTP error! status: ${response.status} - ${rawBody}`)
+      }
+
+      const stream = response.body
+        ?.pipeThrough(new TextDecoderStream())
+        .pipeThrough(OllamaV1Decoder.decoder)
+
+      if (!stream) {
+        throw new Error('ReadableStream not supported by the response.')
+      }
+
+      const reader = stream.getReader()
+      let toolCallingChunk: OllamaV1Chunk | null = null
+
+      while (true) {
+        const { done, value } = await reader.read()
+        if (done) break
+        if (value === '[DONE]') {
+          if (toolCallingChunk != null) {
+            messages.push({
+              content: null,
+              role: LLMRole.assistant,
+              toolCalls: toolCallingChunk.choices[0].delta.toolCalls?.map(
+                (toolCall) => toolCall.toJson()
+              ),
+            })
+
+            for (const toolCall of toolCallingChunk.choices[0].delta
+              .toolCalls ?? []) {
+              const toolCallFunction = toolCall.functionCall
+              const tool = tools?.find((t) => t.name === toolCallFunction.name)
+              const toolResponse =
+                (await tool?.use(
+                  JSON.parse(toolCallFunction.functionArguments)
+                )) ??
+                `Unable to use non-existent tool: ${toolCallFunction.name}`
+
+              messages.push({
+                role: LLMRole.tool,
+                content: toolResponse,
+                toolCallId: toolCall.id,
+              })
+            }
+
+            yield* this.generateStream(model, messages, tools, options)
+          }
+          continue
+        }
+
+        const chunk: OllamaV1Chunk = value
+        const toolCalls = chunk.choices[0].delta.toolCalls
+        if (toolCalls == null) {
+          yield chunk
+        } else {
+          if (toolCallingChunk == null) {
+            toolCallingChunk = chunk
+          } else {
+            toolCallingChunk = toolCallingChunk.copyWith(chunk)
+          }
+        }
+      }
+    }.call(this)
   }
-  generateEmbedding(
+
+  async generateEmbedding(
     embeddingModel: string,
-    chunks: string[]
+    chunks: string[],
+    options?: Record<string, any>
   ): Promise<ChunkEmbedding[]> {
-    throw new Error('Method not implemented.')
+    const body = {
+      model: embeddingModel,
+      input: chunks,
+      options: options,
+    }
+    const response = await fetch(`${this.baseUrl}/api/embed`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+    const rawText = await response.text()
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status} - ${rawText}`)
+    }
+    const embeddingsResponse = OllamaEmbedding.fromJson(JSON.parse(rawText))
+    const embeddings: ChunkEmbedding[] = []
+    for (let i = 0; i < chunks.length; i++) {
+      embeddings.push({
+        chunk: chunks[i],
+        vector: embeddingsResponse.embeddings[i],
+      })
+    }
+    return embeddings
   }
+}
+
+class OllamaV1Decoder {
+  static get decoder(): TransformStream<string, OllamaV1Chunk | '[DONE]'> {
+    return new TransformStream<string, OllamaV1Chunk | '[DONE]'>({
+      start() {
+        this.buffer = '' // Buffer for incomplete JSON lines
+      },
+      transform(chunk, controller) {
+        this.buffer += chunk
+
+        const lines = this.buffer.split('\n')
+        this.buffer = lines.pop() || '' // Keep the last incomplete line in the buffer
+
+        for (const line of lines) {
+          if (line.startsWith('data:')) {
+            const content = line.substring(5).trim()
+            if (content === '[DONE]') {
+              controller.enqueue('[DONE]')
+            } else if (content) {
+              try {
+                const chunk = OllamaV1Chunk.fromJson(JSON.parse(content)) // Convert to OllamaV1Chunk
+                controller.enqueue(chunk) // Output the OllamaV1Chunk instance
+              } catch (error) {
+                console.error('Error parsing JSON:', content, error)
+                // If parsing fails, wait for more data
+                this.buffer += line
+              }
+            }
+          }
+        }
+      },
+      flush(controller) {
+        // Try to process the remaining buffer
+        if (this.buffer.trim()) {
+          try {
+            const jsonObject = JSON.parse(this.buffer.trim())
+            const chunk = OllamaV1Chunk.fromJson(jsonObject)
+            controller.enqueue(chunk)
+          } catch (error) {
+            console.error('Error parsing remaining buffer:', error)
+          }
+        }
+        controller.terminate()
+      },
+    })
+  }
+}
+
+function toOllamaMessages(messages: LLMMessage[] | null): any[] {
+  return messages.map((llmMessage: LLMMessage) => {
+    return {
+      role: LLMRole[llmMessage.role],
+      content: llmMessage.content,
+      tool_calls: llmMessage.toolCalls,
+      tool_call_id: llmMessage.toolCallId,
+      images: llmMessage.images,
+    }
+  })
+}
+
+function toOllamaTools(tools: Tool[] | null): any[] {
+  if (tools == null) return null
+  return tools.map((tool: Tool) => {
+    const properties: Record<string, any> = {}
+
+    for (const param of tool.parameters) {
+      properties[param.name] = {
+        type: param.type,
+        description: param.description,
+        ...(param.enums?.length ? { enum: param.enums } : {}),
+      }
+    }
+    const required = tool.parameters
+      .filter((param) => param.isRequired)
+      .map((param) => param.name)
+
+    const functionDescription = {
+      type: 'function',
+      function: {
+        name: tool.name,
+        description: tool.description,
+        parameters:
+          tool.parameters.length > 0
+            ? {
+                type: 'object',
+                properties: properties, // Correctly assign the properties here
+                additionalProperties: false,
+                ...(required.length > 0 ? { required: required } : {}),
+              }
+            : {},
+      },
+    }
+    return functionDescription
+  })
 }
---
+++ apps/server/tests/ollama.test.ts
@@ -0,0 +1,158 @@
+import { LLMRole, Tool } from '../repository/llm-repository'
+
+import { OllamaLLMRepositoryImpl } from '../implementations/ollama-llm-repository-impl'
+
+let ollamaClient: OllamaLLMRepositoryImpl
+
+class FakeLocationTool extends Tool {
+  name = 'get_location'
+  description =
+    'Lets you find the location of a place in latitude and longitude'
+  parameters = [
+    {
+      name: 'location',
+      description: 'The place you want to find latitude and longitude for',
+      type: 'string',
+      isRequired: true,
+    },
+  ]
+  async use(args?: Record<string, any>): Promise<any> {
+    const location = args['location']
+    if (location == null) {
+      return (
+        'Unable to get location for get_location: ' +
+        JSON.stringify(args, null, 2)
+      )
+    }
+    return 'Location of ' + location + ' is at 34.2 latitude, 23.2 longitude'
+  }
+}
+
+class FakeWeatherTool extends Tool {
+  name = 'get_weather'
+  description = 'Retrieves weather for a given latitude and longitude'
+  parameters = [
+    {
+      name: 'latitude',
+      description: 'The latitude of the place you want weather for',
+      type: 'string',
+      isRequired: true,
+    },
+    {
+      name: 'longitude',
+      description: 'The longitude of the place you want weather for',
+      type: 'string',
+      isRequired: true,
+    },
+  ]
+  async use(args: Record<string, any>): Promise<any> {
+    const latitude: number = args['latitude'] | args['lat']
+    const longitude: number = args['longitude'] | args['lon']
+    if (latitude == null || longitude == null) {
+      return 'Missing latitude or longitude for weather'
+    }
+    return (
+      'The weather at ' +
+      latitude +
+      ' latitude and ' +
+      longitude +
+      ' longitude is snowy and -10 celsius'
+    )
+  }
+}
+
+beforeAll(() => {
+  ollamaClient = new OllamaLLMRepositoryImpl('http://localhost:11434')
+})
+
+test('Simple message with Ollama', async () => {
+  const response = await ollamaClient.generateReply(
+    'llama3.1',
+    [
+      {
+        role: LLMRole.system,
+        content:
+          'You are a helpful pirate AI, talking like a pirate. You are totally illiterate and a science denier. Maximum 5 words in answers',
+      },
+      { role: LLMRole.user, content: 'Tell me why the sky is blue' },
+    ],
+    null,
+    { seed: 10, temperature: 0 }
+  )
+  console.log('Output: ' + response.content)
+  //  expect(response.content).toBe('Magic pixies do it matey!')
+}, 20000)
+
+test('Simple stream with Ollama', async () => {
+  const stream = ollamaClient.generateStream(
+    'llama3.1',
+    [
+      {
+        role: LLMRole.system,
+        content:
+          'You are a helpful Pirate AI, talk like a swashbuckling scallywag. Max 5 words',
+      },
+      { role: LLMRole.user, content: 'Tell me why the sky is blue' },
+    ],
+    null,
+    { seed: 30, temperature: 0 }
+  )
+
+  let accumulatedResponse = ''
+
+  for await (const chunk of stream) {
+    if (chunk.content) {
+      accumulatedResponse += chunk.content ?? ''
+    }
+  }
+  expect(accumulatedResponse.length).toBeGreaterThan(0)
+}, 20000)
+
+test('Create embeddings with Ollama', async () => {
+  const embeddings = await ollamaClient.generateEmbedding('nomic-embed-text', [
+    'This is a test',
+  ])
+  expect(embeddings.length).toBeGreaterThan(0)
+}, 20000)
+
+test('Tool request in Ollama response', async () => {
+  const response = await ollamaClient.generateReply(
+    'llama3.1',
+    [
+      {
+        role: LLMRole.system,
+        content:
+          'You are a helpful weather AI assistant. If possible use provided tool in response only. Never assume location. Use tool responses',
+      },
+      {
+        role: LLMRole.user,
+        content: 'What is the weather in Mengele?',
+      },
+    ],
+    [new FakeLocationTool(), new FakeWeatherTool()]
+  )
+  console.log('Response: ' + JSON.stringify(response.content, null, 2))
+}, 20000)
+
+test('Tool request in Ollama stream', async () => {
+  const stream = ollamaClient.generateStream(
+    'llama3.1',
+    [
+      {
+        role: LLMRole.system,
+        content:
+          'You are a helpful weather AI assistant. If possible use provided tool in response only. Never assume location. Use tool responses',
+      },
+      { role: LLMRole.user, content: 'What is the weather in Mengele?' },
+    ],
+    [new FakeLocationTool(), new FakeWeatherTool()]
+  )
+  let accumulatedResponse = ''
+
+  for await (const chunk of stream) {
+    if (chunk.content) {
+      accumulatedResponse += chunk.content ?? ''
+    }
+  }
+  console.log('Output:' + accumulatedResponse)
+}, 20000)
