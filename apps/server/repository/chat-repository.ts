c3c5113a2a180bb8dc1575a173118041bcd7b9d7
Joacim Delfin Sveen <joacim.sveen@knowit.no>
2024-12-13 11:27:20 +0100

    Updated the interface for ChatRepository.

--- apps/server/repository/chat-repository.ts
+++ apps/server/repository/chat-repository.ts
@@ -1,46 +1,71 @@
-// Repository for communication with KnowitGPT chat and the chats stored in database
-export interface ChatRepository {
-  /*
-  Retrieves a list of chats a user has had with KnowitGPT
-  Args:
-    user_id: the knowit identifier for the user
-    limit: default to a given set to not overload it
-    offset: used for pagination combined with limit
-  Returns:
-    A list of Chat objects
-  */
-  fetchChatsForUser(
-    user_id: string,
-    limit?: number,
-    offset?: number
-  ): Array<Chat>
+// Repository connection for all thecommunication with KnowitGPT chat and the chats stored in database
 
-  // Retrieves the current messages for a given chat_id
-  fetchMessagesForChat(chat_id: string): Array<ChatMessage>
-  /*
-  Sends a new message from user for a given chat.
-  Handling of this includes using RAG as well as communicating with LLM to generate
-  a good stream that answers the question
-  */
-  sendMessage(
-    user_id: string,
+/**
+ * Repository for the communication with KnowitGPT in the form of chats and chat messages.
+ * This interface defines the classes and methods that are necessary to handle database entries regarding communication
+ * with KnowitGPT in the form of chats and chat messages.
+ */
+export interface IChatRepository {
+  /**
+   * Add a chat message for a chat.
+   *
+   * @param chat_id - The identifier of the chat that the message is a part of.
+   * @param user_id - The identifier of the user.
+   * @param message - The text of the chat entry.
+   * @param role - The role of the creator of the entry. It is either the user or the assistant.
+   * @returns `true` if successfully created
+   */
+  addChatMessage(
     chat_id: string,
-    prompt: string
-  ): AsyncGenerator<ChatChunk>
+    user_id: string,
+    message: string,
+    role: ChatRole
+  ): boolean
 
-  // Deletes a chat from log. Returns true if successful. User can only delete his own
-  deleteChatFromHistory(chat_id: string, user_id: string): boolean
+  /**
+   * Delete a chat. Only the user can delete their own chat.
+   *
+   * @param chat_id - The identifier for the chat to be deleted.
+   * @returns `true` if successfully deleted, otherwise `false`.
+   */
+  deleteChat(chat_id: string): boolean
+
+  /**
+   * Retrieve all chats for a user.
+   *
+   * @param user_id - The identifier for the user to retrieve chats for.
+   * @param limit - Default to a given set to not overload it.
+   * @param offset - Used for pagination combined with limit.
+   * @returns All chats with KnowitGPT for the user.
+   */
+  getChatsForUser(user_id: string, limit?: number, offset?: number): Array<Chat>
+
+  /**
+   * Retrieve all the chat messages for a chat.
+   *
+   * @param chat_id - The identifier for the chat to retrieve messages for.
+   * @returns A list of all the chat messages for the chat.
+   */
+  getChatMessagesForChat(chat_id: string): Promise<Array<ChatMessage>>
 }
 
 // A chat user has had with the KnowitGPT
-class Chat {
+export class Chat {
   id: string
+  user_id: string
   created: Date
   lastUpdated: Date
   title: string
 
-  constructor(id: string, created: Date, lastUpdated: Date, title: string) {
+  constructor(
+    id: string,
+    user_id: string,
+    created: Date,
+    lastUpdated: Date,
+    title: string
+  ) {
     this.id = id
+    this.user_id = user_id
     this.created = created
     this.lastUpdated = lastUpdated
     this.title = title
@@ -51,39 +76,29 @@ class Chat {
 export class ChatMessage {
   id: string
   chat_id: string
+  user_id: string
   message: string
   role: ChatRole
-  timestamp: Date
+  created: Date
 
   constructor(
     id: string,
     chat_id: string,
+    user_id: string,
     message: string,
     role: ChatRole,
-    timestamp: Date
+    created: Date
   ) {
     this.id = id
     this.chat_id = chat_id
+    this.user_id = user_id
     this.message = message
     this.role = role
-    this.timestamp = timestamp
-  }
-}
-
-// A single token received from KnowitGPT after a message is sent
-export class ChatChunk {
-  content: string
-  timestamp: Date
-
-  constructor(content: string, timestamp: Date) {
-    this.content = content
-    this.timestamp = timestamp
+    this.created = created
   }
 }
 
 export enum ChatRole {
-  // Message from KnowitGPT
-  assistant,
-  // Message from user
-  user,
+  assistant, // Message from KnowitGPT
+  user, // Message from user
 }
