20901542ae038af9acca70c29d060011b822a1b1
Piotr Kamisinski <37105798+p10trk@users.noreply.github.com>
2024-12-30 12:08:05 +0100

    Fikset

--- apps/web/src/pages/employee/cards/EmployeeMotivationAndCompetenceCard.tsx
+++ apps/web/src/pages/employee/cards/EmployeeMotivationAndCompetenceCard.tsx
@@ -20,12 +20,29 @@ const EmployeeCompetenceCard: FC<Props> = ({ employeeEmail }) => {
   const { data: scoreData, error: scoreError } =
     useEmployeeCompetenceScoreCharts(employeeEmail)
 
-  // Append t-profile chart to the first chart group ("Hovedkategorier")
-  if (motAndCompData && scoreData) {
-    ;(
-      motAndCompData as MultipleChartData<SingularChartData[]>
-    ).groups[0].charts.push(scoreData as TProfileChartData)
+  // Append t-profile chart, unless it exists, to the first chart group ("Hovedkategorier") in motAndCompData.
+  const appendTProfileChartIfNotExists = (motAndCompData, scoreData): void => {
+    const isTProfileChartData = (
+      value: SingularChartData
+    ): value is SingularChartData => value?.type === 'TProfileChart'
+
+    if (motAndCompData && scoreData) {
+      //if (!motAndCompError) {
+      if (
+        !(
+          motAndCompData as MultipleChartData<SingularChartData[]>
+        ).groups[0].charts.some((e) => isTProfileChartData(e))
+      ) {
+        if (!scoreError) {
+          ;(
+            motAndCompData as MultipleChartData<SingularChartData[]>
+          ).groups[0].charts.push(scoreData as TProfileChartData)
+        }
+      }
+    }
   }
+
+  appendTProfileChartIfNotExists(motAndCompData, scoreData)
 
   return (
     <ChartCard
