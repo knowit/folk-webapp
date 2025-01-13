7cea472959d32c53cf715eb14312314ac6623016
Joacim Delfin Sveen <joacim.sveen@knowit.no>
2025-01-13 07:42:35 +0100

    Updated database routers to handle DynamoDb correctly. Upda

--- apps/server/routers/databaseRouter.ts
+++ apps/server/routers/databaseRouter.ts
@@ -1,5 +1,4 @@
 import express, { Router } from 'express'
-import { PostgresChatRepository } from '../implementations/repositories/postgres-chat-repository'
 import { DynamoDBChatRepository } from '../implementations/repositories/dynamodb-chat-repository'
 
 const router: Router = express.Router()
@@ -11,25 +10,23 @@ router.delete('/chats', async (req, res) => {
   res.send(result)
 })
 
-router.get<unknown, unknown, unknown, string>('/chat', async (req, res) => {
-  const result = await db.getChat(req.query['chatId'])
+router.get('/chat', async (req, res) => {
+  const result = await db.getChat(req.body.userId, req.body.chatId)
   res.send(result)
 })
 
-router.get<unknown, unknown, unknown, string>('/chats', async (req, res) => {
-  const result = await db.getChatsForUser(req.query['userId'])
+router.get('/chats', async (req, res) => {
+  const result = await db.getChatsForUser(req.body.userId)
   res.send(result)
 })
 
-router.get<unknown, unknown, unknown, string>(
-  '/chatMessages',
-  async (req, res) => {
-    console.log(req.query['chatId'])
-    const result = await db.getChatMessagesForChat(req.query['chatId'])
-    console.log(result)
-    res.send(result)
-  }
-)
+router.get('/chatMessages', async (req, res) => {
+  const result = await db.getChatMessagesForChat(
+    req.body.userId,
+    req.body.chatId
+  )
+  res.send(result)
+})
 
 router.post('/chat', async (req, res) => {
   const result = await db.addChat(req.body.userId, req.body.title)
@@ -37,7 +34,6 @@ router.post('/chat', async (req, res) => {
 })
 
 router.post('/chatMessages', async (req, res) => {
-  console.log('ny melding')
   const result = await db.addChatMessage(
     req.body.chatId,
     req.body.userId,
@@ -46,12 +42,5 @@ router.post('/chatMessages', async (req, res) => {
   )
   res.send(result)
 })
-
-/*
-router.post('/setup', async (_, res) => {
-  const result = await db.setupPostgres()
-  res.send(result)
-})
- */
 
 export { router as databaseRouter }
